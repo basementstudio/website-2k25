b9e79b450285ccae0c16b9d2ec5cc8f8e1fe6d16
Tomas Ferreras <tomas@basement.studio>
2024-11-27 10:50:26 -0300

    chore: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { Demo } from "@/components/demo";
-import { Pump } from "basehub/react-pump";
-import { RichText } from "basehub/react-rich-text";
 
 const Homepage = () => (
   <div className="flex h-screen flex-col items-center justify-center gap-8 bg-black p-12 text-white">
