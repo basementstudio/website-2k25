22beaa6f84c5e5b654e34f829e6a684e1fbe4b0a
Vittorio Retrivi <vittorioretrivi@gmail.com>
2024-12-10 18:29:54 -0300

    fix build

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,15 @@ const nextConfig: NextConfig = {
       },
     },
   },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
