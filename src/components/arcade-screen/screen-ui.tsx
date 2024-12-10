e2a0e2cd5150cd566f32506faea31a1c38f4a89d
tomasfdev <hellotomasdev@gmail.com>
2024-12-10 13:46:15 -0300

    feat: add ts ignore file jsx error

--- src/components/arcade-screen/screen-ui.tsx
+++ src/components/arcade-screen/screen-ui.tsx
@@ -1,37 +1,10 @@
+// @ts-nocheck
 import {
   Container,
   Root,
   Text,
 } from '@react-three/uikit';
-import basement from './basement.png';
 import { Vector2 } from 'three';
-
-
-
-const LABS_DATA = [
-  {
-    title: 'Instanced grass',
-    description: 'Instanced grass',
-    image:  "",
-    contributors: ['/git-chad'],
-    link: 'https://lab.basement.studio/experiments/77.instanced-grass',
-  },
-  {
-    title: 'Shader matcap transition',
-    description:
-      'Animate transition between matcap texture and shader based on scroll.',
-    image:  "",
-    contributors: ['/tomasferrerasdev', '/matiasngf'],
-    link: 'https://lab.basement.studio/experiments/75.shader-matcap-transition.tsx',
-  },
-  {
-    title: 'Butterfly Particle Sphere',
-    description: `This example is based on this other example to show how splines can be used to create a camera rail. The camera will follow the curve and rotate to face the tangent's direction of the current point in the curve. It also modifies the target view in certain points of the curve to face other desired targets.`,
-    image:  "",
-    contributors: ['/ignmandagaran'],
-    link: 'https://lab.basement.studio/experiments/76.butterfly-particle-sphere',
-  },
-];
 
 interface ScreenUIProps {
   dimensions?: Vector2;
