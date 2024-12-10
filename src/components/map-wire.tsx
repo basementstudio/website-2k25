b54e2c83725d41134bf5ccfc412e7a632234e07c
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-12-10 17:35:05 -0300

    memo map wire

--- src/components/map-wire.tsx
+++ src/components/map-wire.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import { useGLTF } from "@react-three/drei";
 import { GLTFResult } from "./map";
-import { Mesh, MeshStandardMaterial } from "three";
+import { Mesh, MeshStandardMaterial, ShaderMaterial } from "three";
 import { createShaderMaterial } from "@/shaders/custom-shader-material";
+import { memo, useEffect, useMemo, useState } from "react";
 
-export const MapWire = () => {
+export const MapWire = memo(MapWireInner);
+
+function MapWireInner() {
   const { nodes } = useGLTF("/models/map-wire.glb") as unknown as GLTFResult;
-  const material = createShaderMaterial(
-    (nodes.WireFrame_MeshCurveMesh as Mesh).material as MeshStandardMaterial,
-    true,
-  );
+  const [material, setMaterial] = useState<ShaderMaterial | null>(null);
+
+  useEffect(() => {
+    setMaterial(
+      createShaderMaterial(
+        (nodes.WireFrame_MeshCurveMesh as Mesh)
+          .material as MeshStandardMaterial,
+        true,
+      ),
+    );
+  }, [nodes.WireFrame_MeshCurveMesh]);
+
+  if (!material) return null;
 
   return (
     <group dispose={null} name="map-wire">
@@ -18,6 +32,6 @@ export const MapWire = () => {
       />
     </group>
   );
-};
+}
 
 useGLTF.preload("/models/map-wire.glb");
