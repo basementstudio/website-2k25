7be9d7a1f95e883186ac7923082e677e36d5f37b
git-chad <tobimocc@gmail.com>
2024-12-11 15:24:04 -0300

    fix merge

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -7,7 +7,7 @@ import { Environment } from "@react-three/drei";
 import { Map } from "@/components/map";
 import { Inspectables } from "@/components/inspectables/inspectables";
 import { MapWire } from "./map-wire";
-  
+
 import { Renderer } from "./postprocessing/renderer";
 import { Leva } from "leva";
 
@@ -15,18 +15,18 @@ export const Scene = () => (
   <div className="absolute inset-0">
     <Leva />
     <Canvas gl={{ antialias: true, alpha: false }} camera={{ fov: 45 }}>
-        <Renderer
-          sceneChildren={
-            <>
-              <color attach="background" args={["#000"]} />
-              <CustomCamera />
-              <Map />
-              <MapWire />
-              <Inspectables />
-              <Environment preset="studio" />
-            </>
-          }
-        />
+      <Renderer
+        sceneChildren={
+          <>
+            <color attach="background" args={["#000"]} />
+            <CustomCamera />
+            <Map />
+            <MapWire />
+            <Inspectables />
+            <Environment preset="studio" />
+          </>
+        }
+      />
     </Canvas>
   </div>
 );
