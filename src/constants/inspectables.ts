e3284599a5bb84c495fa9362b458d18f6ab76048
Vittorio Retrivi <vittorioretrivi@gmail.com>
2024-12-11 14:12:33 -0300

    shelf objects interactions (#10)

---
+++ src/constants/inspectables.ts
@@ -0,0 +1,12 @@
+import { ValueAnimationTransition } from "motion/react";
+
+export const TARGET_SIZE = 0.5;
+export const SMOOTH_FACTOR = 0.1;
+export const X_OFFSET = 0.13;
+
+export const ANIMATION_CONFIG: ValueAnimationTransition = {
+  stiffness: 100,
+  damping: 20,
+  restDelta: 0.001,
+  type: "spring",
+};
