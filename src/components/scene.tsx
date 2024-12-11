c93b0d097f25b9bf6002444a1457a70af3a6c1ef
Tobi Moccagatta <tobimocc@gmail.com>
2024-12-11 15:55:36 -0300

    Tobi/postprocessing dither (#9)
    * init
    
    * ...
    
    * setup ready
    
    * dither
    
    * scene dithering + leva tweaks
    
    * initial values
    
    * tolerance for neighbor colors
    
    * dither improvements
    
    * mat16x16, more improvements
    
    * color preservation, bayer/crosshatch
    
    * yaml uwu
    
    * missing import
    
    * ...
    
    * dither bayer with texture
    
    * fixed pixel size step
    
    * fix texture loading'
    
    * blue noise + bayer 16x16 texture
    
    * fix merge
    
    * moved decl

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -3,19 +3,30 @@
 import { Canvas } from "@react-three/fiber";
 import { CustomCamera } from "@/components/camera-controls";
 import { Environment } from "@react-three/drei";
+
 import { Map } from "@/components/map";
 import { Inspectables } from "@/components/inspectables/inspectables";
 import { MapWire } from "./map-wire";
 
+import { Renderer } from "./postprocessing/renderer";
+import { Leva } from "leva";
+
 export const Scene = () => (
   <div className="absolute inset-0">
+    <Leva />
     <Canvas gl={{ antialias: true, alpha: false }} camera={{ fov: 45 }}>
-      <color attach="background" args={["#000"]} />
-      <CustomCamera />
-      <Map />
-      <MapWire />
-      <Inspectables />
-      <Environment preset="studio" />
+      <Renderer
+        sceneChildren={
+          <>
+            <color attach="background" args={["#000"]} />
+            <CustomCamera />
+            <Map />
+            <MapWire />
+            <Inspectables />
+            <Environment preset="studio" />
+          </>
+        }
+      />
     </Canvas>
   </div>
 );
