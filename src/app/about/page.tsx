10479758ac57b9226abc967f804c76709b5739e8
Vittorio Retrivi <vittorioretrivi@gmail.com>
2024-12-13 11:47:55 -0300

    minor: sort awards

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -243,25 +243,30 @@ const About = () => (
               x25
             </p>
             <ul className="relative col-start-1 col-end-13 row-start-1 text-paragraph text-brand-w1">
-              {data.company.awards.awardList.items.map((award) => (
-                <li
-                  key={award._id}
-                  className="group relative grid grid-cols-12 gap-2 [&:first-child>.item]:border-t"
-                >
-                  <div className="item col-start-5 col-end-13 grid grid-cols-4 gap-2 border-b border-brand-w1/20 pb-0.5 pt-px">
-                    <div className="col-span-1">{award.title}</div>
-                    <div className="col-span-1">{award.project._title}</div>
-                    <div className="col-span-1">
-                      {new Date(award.date).toLocaleDateString("en-US", {
-                        month: "long",
-                        day: "numeric",
-                        year: "numeric",
-                      })}
+              {data.company.awards.awardList.items
+                .sort(
+                  (a, b) =>
+                    new Date(b.date).getTime() - new Date(a.date).getTime(),
+                )
+                .map((award) => (
+                  <li
+                    key={award._id}
+                    className="group relative grid grid-cols-12 gap-2 [&:first-child>.item]:border-t"
+                  >
+                    <div className="item col-start-5 col-end-13 grid grid-cols-4 gap-2 border-b border-brand-w1/20 pb-0.5 pt-px">
+                      <div className="col-span-1">{award.title}</div>
+                      <div className="col-span-1">{award.project._title}</div>
+                      <div className="col-span-1">
+                        {new Date(award.date).toLocaleDateString("en-US", {
+                          month: "long",
+                          day: "numeric",
+                          year: "numeric",
+                        })}
+                      </div>
                     </div>
-                  </div>
-                  <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
-                </li>
-              ))}
+                    <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+                  </li>
+                ))}
             </ul>
           </div>
         </main>
