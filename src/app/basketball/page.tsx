87de270fc176318a990ffd505b1e0165c8fe661e
git-chad <tobimocc@gmail.com>
2024-12-18 12:37:07 -0300

    removed basketball page

--- src/app/basketball/page.tsx
+++ src/app/basketball/page.tsx
@@ -1,7 +1,5 @@
-const Basketball = () => (
-  <main className="relative -mt-24 min-h-96 w-full bg-brand-k px-4 pt-2">
-    <h1 className="text-heading uppercase text-brand-w2">Basketball</h1>
-  </main>
-);
+const Basketball = () => {
+  return null
+}
 
-export default Basketball;
+export default Basketball
