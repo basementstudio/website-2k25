92e75d0a337a89e0778d3991db425383448f16fb
Tomas Ferreras <tomas@basement.studio>
2025-01-03 15:25:00 -0300

    feat: add open positions

--- src/app/careers/open-positions.tsx
+++ src/app/careers/open-positions.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link"
 import { QueryType } from "./careers-query"
+import { cn } from "@/utils/cn"
 
 export const OpenPositions = ({ data }: { data: QueryType }) => {
   return (
     <section className="grid-layout">
-      <h2 className="relative z-20 col-start-1 col-end-4 row-start-1 text-heading uppercase text-brand-w2">
+      <h2 className="relative z-20 col-start-1 col-end-4 row-start-1 pt-5 text-heading uppercase text-brand-w2">
         <span className="block">OPEN</span>
         <span className="block">POSITIONS</span>
       </h2>
 
-      <ul className="relative col-start-1 col-end-13 row-start-1 text-paragraph text-brand-w1">
+      <ul className="relative col-start-1 col-end-13 row-start-1 text-paragraph text-brand-w2">
+        <li className="group relative grid grid-cols-12 gap-2">
+          <div className="relative col-start-5 col-end-13 grid grid-cols-8 items-center gap-2 pb-2 text-brand-g1 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w1/20">
+            <span className="col-start-1 col-end-5 text-paragraph">Role</span>
+            <span className="col-start-5 col-end-7 text-paragraph">Type</span>
+            <span className="text-paragraph">Location</span>
+          </div>
+        </li>
         {data.company.openPositions.openPositionsList.items.map(
           ({ _title, text, text_1, boolean }, idx) => (
-            <li
-              key={idx}
-              className="group relative grid grid-cols-12 gap-2 [&:first-child>.item]:after:absolute [&:first-child>.item]:after:-top-px [&:first-child>.item]:after:left-0 [&:first-child>.item]:after:w-full [&:first-child>.item]:after:border-t [&:first-child>.item]:after:border-brand-w1/20"
-            >
+            <li key={idx} className="group relative grid grid-cols-12 gap-2">
               <Link
                 href={""}
-                className="item relative col-start-5 col-end-13 grid grid-cols-8 items-center gap-2 py-2 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w1/20"
+                className={cn(
+                  "relative col-start-5 col-end-13 grid grid-cols-8 items-center gap-2 py-2 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w1/20",
+                  boolean && "text-brand-w2/20"
+                )}
               >
                 <span className="col-start-1 col-end-5 text-subheading">
                   {_title}
@@ -33,7 +41,9 @@ export const OpenPositions = ({ data }: { data: QueryType }) => {
                   </span>
                 </div>
               </Link>
-              <div className="with-diagonal-lines pointer-events-none !absolute -top-px bottom-0 left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+              {!boolean && (
+                <div className="with-diagonal-lines pointer-events-none !absolute -top-px bottom-0 left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+              )}
             </li>
           )
         )}
