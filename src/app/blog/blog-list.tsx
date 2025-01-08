37f10e87c0408f67ccc1bb6925cb840cc99cdebb
git-chad <tobimocc@gmail.com>
2025-01-08 11:14:22 -0300

    links

--- src/app/blog/blog-list.tsx
+++ src/app/blog/blog-list.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link"
+
 import { QueryType } from "./query"
 
 interface BlogListProps {
@@ -35,30 +37,35 @@ export default function BlogList({
         {posts.map((post) => (
           <div
             key={post._slug}
-            className="group relative col-span-12 grid grid-cols-12 gap-2 border-b border-brand-w1/20 py-2"
+            className="group relative col-span-12 border-b border-brand-w1/20 py-2"
           >
-            <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
-            <div className="with-dots col-span-2 h-[124px] w-[276px] bg-brand-g2/20" />
-            <p className="col-start-5 col-end-8 text-subheading text-brand-w2">
-              {post._title}
-            </p>
-            <div className="col-span-2 col-start-9 flex gap-1">
-              {post.categories?.map((category) => (
-                <p
-                  key={category._title}
-                  className="h-max w-max bg-brand-g2 px-1 text-paragraph text-brand-w2"
-                >
-                  {category._title}
-                </p>
-              ))}
-            </div>
-            <p className="col-span-1 col-start-11 text-paragraph text-brand-w2">
-              {new Date(post.date || "").toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric",
-                year: "numeric"
-              })}
-            </p>
+            <Link
+              className="col-span-12 grid grid-cols-12 gap-2"
+              href={`/blog/${post._slug}`}
+            >
+              <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+              <div className="with-dots col-span-2 h-[124px] w-[276px] bg-brand-g2/20" />
+              <p className="col-start-5 col-end-8 text-subheading text-brand-w2">
+                {post._title}
+              </p>
+              <div className="col-span-2 col-start-9 flex gap-1">
+                {post.categories?.map((category) => (
+                  <p
+                    key={category._title}
+                    className="h-max w-max bg-brand-g2 px-1 text-paragraph text-brand-w2"
+                  >
+                    {category._title}
+                  </p>
+                ))}
+              </div>
+              <p className="col-span-1 col-start-11 text-paragraph text-brand-w2">
+                {new Date(post.date || "").toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric"
+                })}
+              </p>
+            </Link>
           </div>
         ))}
       </div>
--- src/app/blog/featured.tsx
+++ src/app/blog/featured.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { RichText } from "basehub/react-rich-text"
+import Link from "next/link"
 
 import { Filters } from "./filters"
 import { QueryType } from "./query"
@@ -51,37 +52,42 @@ export default function Featured({
         {featuredPosts.map((post) => (
           <div
             key={post._slug}
-            className="group relative col-span-12 grid grid-cols-12 gap-2 border-b border-brand-w1/20 py-2"
+            className="group relative col-span-12 border-b border-brand-w1/20 py-2"
           >
-            <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+            <Link
+              className="col-span-12 grid grid-cols-12 gap-2"
+              href={`/blog/${post._slug}`}
+            >
+              <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
 
-            <div className="with-dots col-span-3 h-[228px] w-[418px] bg-brand-g2/20" />
-            <h2 className="col-start-5 col-end-8 text-subheading text-brand-w2">
-              {post._title}
-            </h2>
-            <div className="relative col-span-4 col-start-9 grid grid-cols-4 content-start gap-y-2">
-              <div className="col-start-1 col-end-4 block text-paragraph text-brand-w2">
-                <RichText content={post.intro?.json.content} />
+              <div className="with-dots col-span-3 h-[228px] w-[418px] bg-brand-g2/20" />
+              <h2 className="col-start-5 col-end-8 text-subheading text-brand-w2">
+                {post._title}
+              </h2>
+              <div className="relative col-span-4 col-start-9 grid grid-cols-4 content-start gap-y-2">
+                <div className="col-start-1 col-end-4 block text-paragraph text-brand-w2">
+                  <RichText content={post.intro?.json.content} />
+                </div>
+                <hr className="col-start-1 col-end-5 mt-2 w-full border-brand-w1/20" />
+                <div className="col-span-2 col-start-1 flex gap-1">
+                  {post.categories?.map((category) => (
+                    <p
+                      key={category._title}
+                      className="h-max w-max bg-brand-g2 px-1 text-paragraph text-brand-w2"
+                    >
+                      {category._title}
+                    </p>
+                  ))}
+                </div>
+                <p className="col-span-2 col-start-3 text-paragraph text-brand-w2">
+                  {new Date(post.date || "").toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric"
+                  })}
+                </p>
               </div>
-              <hr className="col-start-1 col-end-5 mt-2 w-full border-brand-w1/20" />
-              <div className="col-span-2 col-start-1 flex gap-1">
-                {post.categories?.map((category) => (
-                  <p
-                    key={category._title}
-                    className="h-max w-max bg-brand-g2 px-1 text-paragraph text-brand-w2"
-                  >
-                    {category._title}
-                  </p>
-                ))}
-              </div>
-              <p className="col-span-2 col-start-3 text-paragraph text-brand-w2">
-                {new Date(post.date || "").toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric"
-                })}
-              </p>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
