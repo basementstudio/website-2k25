846098d6efa10d4a7eca94170bb5c6d4c6dac621
git-chad <tobimocc@gmail.com>
2025-01-10 18:32:06 -0300

    point camera downwards

--- src/constants/camera-states.ts
+++ src/constants/camera-states.ts
@@ -20,8 +20,8 @@ export const CAMERA_STATES: Record<CameraStateKeys, CameraState> = {
     name: "arcade",
     url: "/lab",
     object_name: "arcade",
-    position: [2.98, 1.39, -12.8],
-    target: [2.98, 1.35, -13.8],
+    position: [2.98, 1.6, -12.8],
+    target: [2.98, 1.33, -13.8],
     rotationAngle: [0, 0],
     rotationLerp: 0
   },
