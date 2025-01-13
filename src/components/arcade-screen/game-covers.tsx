0abcef3f8a9ec89b07fbbffd6edd9ae78c93d3f8
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-01-13 13:57:52 -0300

    Arcade Updates (#37)

--- src/components/arcade-screen/game-covers.tsx
+++ src/components/arcade-screen/game-covers.tsx
@@ -5,119 +5,102 @@ import React from "react"
 import { COLORS_THEME } from "./screen-ui"
 import { TextTag } from "./text-tags"
 
-export const GameCovers = () => {
-  return (
-    <>
-      <Container width={"100%"} height={"35%"} paddingTop={16}>
+export const GameCovers = () => (
+  <Container width="100%" height="35%" paddingTop={16}>
+    <Container height={20} width="100%" paddingTop={10} positionType="absolute">
+      <Separator
+        orientation="horizontal"
+        backgroundColor={COLORS_THEME.primary}
+        height={2}
+      />
+      <Container
+        positionType="absolute"
+        width="100%"
+        height={16}
+        positionTop={0}
+        positionLeft={0}
+      >
+        <Container width="65%" height="100%" positionType="relative">
+          <TextTag text="Arcade" icon />
+        </Container>
+      </Container>
+    </Container>
+    <Container
+      width="100%"
+      height="100%"
+      paddingTop={32}
+      paddingX={14}
+      paddingBottom={16}
+    >
+      <Container
+        width="100%"
+        height="100%"
+        borderWidth={3}
+        borderColor={COLORS_THEME.primary}
+      >
         <Container
-          height={20}
-          width={"100%"}
-          paddingTop={10}
-          positionType={"absolute"}
+          width="50%"
+          height="100%"
+          positionType="relative"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          onClick={() => {
+            window.open(`https://chronicles.basement.studio/`, "_blank")
+          }}
         >
-          <Separator
-            orientation="horizontal"
-            backgroundColor={COLORS_THEME.primary}
-            height={2}
+          <Image
+            src={`/images/arcade-screen/chronicles.jpg`}
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            positionType="absolute"
           />
           <Container
-            positionType={"absolute"}
-            width={"100%"}
-            height={16}
-            positionTop={0}
-            positionLeft={0}
+            height={38}
+            width="auto"
+            backgroundColor={COLORS_THEME.black}
+            zIndexOffset={1}
+            padding={8}
           >
-            <Container width={"65%"} height={"100%"} positionType={"relative"}>
-              <TextTag text="Arcade" icon />
-            </Container>
+            <Text fontSize={20} fontWeight="bold" color={COLORS_THEME.primary}>
+              Play Basment Chronicles
+            </Text>
           </Container>
         </Container>
+        <Separator
+          orientation="vertical"
+          backgroundColor={COLORS_THEME.primary}
+          width={3}
+        />
         <Container
-          width={"100%"}
-          height={"100%"}
-          paddingTop={32}
-          paddingX={14}
-          paddingBottom={16}
+          width="50%"
+          height="100%"
+          positionType="relative"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
         >
+          <Image
+            src={`/images/arcade-screen/looper.jpg`}
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            positionType="absolute"
+          />
           <Container
-            width={"100%"}
-            height={"100%"}
-            borderWidth={3}
-            borderColor={COLORS_THEME.primary}
+            height={38}
+            width="auto"
+            backgroundColor={COLORS_THEME.black}
+            zIndexOffset={1}
+            padding={8}
           >
-            <Container
-              width={"50%"}
-              height={"100%"}
-              positionType={"relative"}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              onClick={() => {
-                window.open(`https://chronicles.basement.studio/`, "_blank")
-              }}
-            >
-              <Image
-                src={`/images/arcade-screen/chronicles.jpg`}
-                width={"100%"}
-                height={"100%"}
-                objectFit={"cover"}
-                positionType={"absolute"}
-              />
-              <Container
-                height={38}
-                width={"auto"}
-                backgroundColor={COLORS_THEME.black}
-                zIndexOffset={1}
-                padding={8}
-              >
-                <Text
-                  fontSize={20}
-                  fontWeight={"bold"}
-                  color={COLORS_THEME.primary}
-                >
-                  Play Basment Chronicles
-                </Text>
-              </Container>
-            </Container>
-            <Separator
-              orientation="vertical"
-              backgroundColor={COLORS_THEME.primary}
-              width={3}
-            />
-            <Container
-              width={"50%"}
-              height={"100%"}
-              positionType={"relative"}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-            >
-              <Image
-                src={`/images/arcade-screen/looper.jpg`}
-                width={"100%"}
-                height={"100%"}
-                objectFit={"cover"}
-                positionType={"absolute"}
-              />
-              <Container
-                height={38}
-                width={"auto"}
-                backgroundColor={COLORS_THEME.black}
-                zIndexOffset={1}
-                padding={8}
-              >
-                <Text
-                  fontSize={20}
-                  fontWeight={"bold"}
-                  color={COLORS_THEME.primary}
-                >
-                  Looper (coming soon)
-                </Text>
-              </Container>
-            </Container>
+            <Text fontSize={20} fontWeight="bold" color={COLORS_THEME.primary}>
+              Looper (coming soon)
+            </Text>
           </Container>
         </Container>
       </Container>
-    </>
-  )
-}
+    </Container>
+  </Container>
+)
--- src/components/arcade-screen/text-tags.tsx
+++ src/components/arcade-screen/text-tags.tsx
@@ -2,38 +2,36 @@ import { Container, Icon, Text } from "@react-three/uikit"
 
 import { COLORS_THEME } from "./screen-ui"
 
-export const TextTag = ({ text, icon }: { text: string; icon?: boolean }) => {
-  return (
-    <Container
-      display={"flex"}
-      flexDirection={"row"}
-      gap={10}
-      positionType={"absolute"}
+export const TextTag = ({ text, icon }: { text: string; icon?: boolean }) => (
+  <Container
+    display="flex"
+    flexDirection="row"
+    gap={10}
+    positionType="absolute"
+    backgroundColor={COLORS_THEME.black}
+    zIndexOffset={10}
+    paddingX={8}
+    positionTop={-3}
+    positionLeft={10}
+  >
+    <Text
+      fontSize={20}
+      color={COLORS_THEME.primary}
+      fontWeight="bold"
       backgroundColor={COLORS_THEME.black}
-      zIndexOffset={10}
-      paddingX={8}
-      positionTop={-3}
-      positionLeft={10}
     >
-      <Text
-        fontSize={20}
+      {text}
+    </Text>
+    {icon && (
+      <Icon
         color={COLORS_THEME.primary}
-        fontWeight={"bold"}
-        backgroundColor={COLORS_THEME.black}
-      >
-        {text}
-      </Text>
-      {icon && (
-        <Icon
-          color={COLORS_THEME.primary}
-          text={`<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 4.8L1.99835e-07 0L2.28571 1.04905e-07L2.28571 2.4L4.57143 2.4V4.8L6.85714 4.8V7.2H9.14286V4.8H11.4286V2.4H13.7143V6.29446e-07L16 7.34351e-07V4.8H13.7143V7.2H11.4286V9.6H9.14286L9.14286 12H6.85714L6.85714 9.6H4.57143L4.57143 7.2H2.28571L2.28571 4.8H0Z" fill="#F68300"/></svg>`}
-          svgWidth={16}
-          svgHeight={16}
-          width={16}
-          height={16}
-          marginTop={6}
-        />
-      )}
-    </Container>
-  )
-}
+        text={`<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 4.8L1.99835e-07 0L2.28571 1.04905e-07L2.28571 2.4L4.57143 2.4V4.8L6.85714 4.8V7.2H9.14286V4.8H11.4286V2.4H13.7143V6.29446e-07L16 7.34351e-07V4.8H13.7143V7.2H11.4286V9.6H9.14286L9.14286 12H6.85714L6.85714 9.6H4.57143L4.57143 7.2H2.28571L2.28571 4.8H0Z" fill="#fff"/></svg>`}
+        svgWidth={16}
+        svgHeight={16}
+        width={16}
+        height={16}
+        marginTop={6}
+      />
+    )}
+  </Container>
+)
