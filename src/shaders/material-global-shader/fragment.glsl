dd8901756b02308d4e36b80d87801b8712f6321f
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-01-16 14:34:04 -0300

    hide the godrays mesh until we have a POC

--- src/shaders/material-global-shader/fragment.glsl
+++ src/shaders/material-global-shader/fragment.glsl
@@ -50,8 +50,9 @@ uniform float fogDensity;
 uniform float fogDepth;
 
 // Glass
-uniform bool isGlass;
+#ifdef GLASS
 uniform sampler2D glassReflex;
+#endif
 
 // Basketball
 uniform bool isBasketball;
@@ -148,14 +149,20 @@ void main() {
 
   gl_FragColor = vec4(irradiance, opacityResult);
 
-  if(isGlass) {
-    // TODO: when implementing parallax and multiple reflections, add controls in basehub to resize the reflection map
-    vec4 reflexSample = texture2D(glassReflex, vUv * vec2(0.75, 1.0));
-    gl_FragColor.rgb = mix(gl_FragColor.rgb, reflexSample.rgb, 0.1);
+  #ifdef GLASS
+  // TODO: when implementing parallax and multiple reflections, add controls in basehub to resize the reflection map
+  vec4 reflexSample = texture2D(glassReflex, vUv * vec2(0.75, 1.0));
+  gl_FragColor.rgb = mix(gl_FragColor.rgb, reflexSample.rgb, 0.1);
 
-    vec2 checkerPos = floor(gl_FragCoord.xy * 0.5);
-    gl_FragColor.a *= mod(checkerPos.x + checkerPos.y, 2.0);
-  }
+  vec2 checkerPos = floor(gl_FragCoord.xy * 0.5);
+  gl_FragColor.a *= mod(checkerPos.x + checkerPos.y, 2.0);
+  #endif
+
+  #ifdef GODRAY
+  vec2 checkerPos = floor(gl_FragCoord.xy * 0.5);
+  // gl_FragColor.a *= mod(checkerPos.x + checkerPos.y, 2.0);
+  gl_FragColor.a = 0.0;
+  #endif
 
   // Fog
   float basketballFogDensity = 0.25;
