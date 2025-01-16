fdece88e747f583dabe300d17d3867103b6098b5
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-01-16 12:15:43 -0300

    Minors

--- src/components/layout/stay-connected.tsx
+++ src/components/layout/stay-connected.tsx
@@ -4,34 +4,32 @@ import { RichText } from "basehub/react-rich-text"
 import { Input } from "@/components/primitives/input"
 import { cn } from "@/utils/cn"
 
+import { Arrow } from "../primitives/icons/arrow"
+
 interface StayConnectedProps {
   content: RichTextNode[]
   className?: string
 }
 
 export const StayConnected = ({ content, className }: StayConnectedProps) => (
-  <div className={cn("grid grid-cols-4 gap-3", className)}>
-    <RichText
-      content={content}
-      components={{
-        h3: ({ children }) => (
-          <p className="col-span-4 text-subheading font-semibold text-brand-w2">
-            {children}
-          </p>
-        ),
-        p: ({ children }) => (
-          <p className="col-start-1 col-end-4 text-paragraph font-semibold text-brand-w2">
-            {children}
-          </p>
-        )
-      }}
-    />
-    <form className="col-span-4 flex flex-col gap-3">
-      <div className="flex flex-col gap-2">
-        <Input className="col-span-4" placeholder="Enter your email" />
-      </div>
-      <button className="col-span-4 w-fit text-h4 text-brand-g1">
-        Roll me in →
+  <div className={cn("flex flex-col gap-6", className)}>
+    <div className="grid grid-cols-4 gap-2">
+      <RichText
+        content={content}
+        components={{
+          h3: ({ children }) => (
+            <p className="col-span-3 text-h3 text-brand-w2">{children}</p>
+          ),
+          p: ({ children }) => (
+            <p className="col-span-3 text-p text-brand-w2">{children}</p>
+          )
+        }}
+      />
+    </div>
+    <form className="flex flex-col gap-4">
+      <Input placeholder="Enter your Email" />
+      <button className="flex w-fit gap-1 text-h4 text-brand-g1">
+        Roll me in <Arrow className="size-5" />
       </button>
     </form>
   </div>
