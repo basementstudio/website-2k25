ab7f6658dbd657c6e02339da87eb9f1f0219d2ed
Tomas Ferreras <tomas@basement.studio>
2025-01-20 22:25:12 +0000

    feat: set cursor hover text null each time a route changes

--- src/components/camera-route-handler.tsx
+++ src/components/camera-route-handler.tsx
@@ -4,15 +4,18 @@ import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 
 import { useCameraStore } from "@/store/app-store"
+import { useMouseStore } from "./mouse-tracker/mouse-tracker"
 
 export const CameraRouteHandler = () => {
   const pathname = usePathname()
+  const setHoverText = useMouseStore((state) => state.setHoverText)
 
   const updateCameraFromPathname = useCameraStore(
     (state) => state.updateCameraFromPathname
   )
 
   useEffect(() => {
+    setHoverText(null)
     updateCameraFromPathname(pathname)
   }, [pathname, updateCameraFromPathname])
 
