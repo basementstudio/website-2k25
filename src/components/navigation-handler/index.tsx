c85a7ab195d49f1e4b0d4fa6e8166ed87ac99fc0
Tomas Ferreras <tomas@basement.studio>
2025-01-25 18:56:06 +0000

    feat: add current scene to navigation store

--- src/components/navigation-handler/index.tsx
+++ src/components/navigation-handler/index.tsx
@@ -3,15 +3,32 @@ import { useEffect } from "react"
 import { useAssets } from "../assets-provider"
 import { useNavigationStore } from "./navigation-store"
 import { IScene } from "./navigation.interface"
+import { usePathname } from "next/navigation"
 
 export const NavigationHandler = () => {
+  const pathname = usePathname()
+
+  // Add scenes to store
   const scenes: IScene[] = useAssets().scenes
   const setScenes = useNavigationStore((state) => state.setScenes)
-
   useEffect(() => {
     setScenes(scenes)
   }, [scenes])
 
-  console.log(scenes)
+  // Add current scene to store
+  const setCurrentScene = useNavigationStore((state) => state.setCurrentScene)
+  useEffect(() => {
+    if (!scenes.length) return
+
+    const currentScene =
+      pathname === "/"
+        ? scenes.find((scene) => scene.name.toLowerCase() === "home")
+        : scenes.find((scene) => scene.name === pathname.split("/")[1])
+
+    if (currentScene) {
+      setCurrentScene(currentScene)
+    }
+  }, [pathname, scenes])
+
   return <></>
 }
--- src/components/navigation-handler/navigation-store.ts
+++ src/components/navigation-handler/navigation-store.ts
@@ -4,7 +4,13 @@ import { IScene } from "./navigation.interface"
 export const useNavigationStore = create<{
   scenes: IScene[] | null
   setScenes: (scenes: IScene[]) => void
+
+  currentScene: IScene | null
+  setCurrentScene: (scene: IScene) => void
 }>((set) => ({
   scenes: null,
-  setScenes: (scenes) => set({ scenes })
+  setScenes: (scenes) => set({ scenes }),
+
+  currentScene: null,
+  setCurrentScene: (scene) => set({ currentScene: scene })
 }))
