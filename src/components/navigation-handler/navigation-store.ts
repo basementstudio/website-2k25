3e5f8eff8b127837063eb8c2186449bb770d5496
Tomas Ferreras <tomas@basement.studio>
2025-01-26 13:29:14 +0000

    feat: add camera store data to remove

--- src/components/navigation-handler/navigation-store.ts
+++ src/components/navigation-handler/navigation-store.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand"
 import { IScene } from "./navigation.interface"
+import { PerspectiveCamera } from "three"
+import { CAMERA_STATES } from "@/constants/camera-states"
 
 export const useNavigationStore = create<{
   scenes: IScene[] | null
@@ -7,10 +9,30 @@ export const useNavigationStore = create<{
 
   currentScene: IScene | null
   setCurrentScene: (scene: IScene) => void
+
+  camera: PerspectiveCamera | null
+  setCamera: (camera: PerspectiveCamera) => void
+  orbitCamera: PerspectiveCamera | null
+
+  //remove this
+  postProcessingCamera: PerspectiveCamera | null
+  disablePostprocessing: boolean
+  setDisablePostprocessing: (value: boolean) => void
 }>((set) => ({
   scenes: null,
   setScenes: (scenes) => set({ scenes }),
 
   currentScene: null,
-  setCurrentScene: (scene) => set({ currentScene: scene })
+  setCurrentScene: (scene) => set({ currentScene: scene }),
+
+  //TODO: remove this
+  activeCamera: "main",
+  cameraState: "home",
+  cameraConfig: CAMERA_STATES.home,
+  camera: null,
+  setCamera: (camera) => set({ camera }),
+  orbitCamera: null,
+  postProcessingCamera: null,
+  disablePostprocessing: false,
+  setDisablePostprocessing: (value) => set({ disablePostprocessing: value })
 }))
