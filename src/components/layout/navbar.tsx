734d0a300f84b0521151220ebcc5ffe5e413f08d
Tomas Ferreras <tomas@basement.studio>
2025-01-26 18:35:59 +0000

    feat: rename careers to people

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => (
         },
         {
           title: "People",
-          href: "/careers"
+          href: "/people"
         },
         {
           title: "Laboratory",
