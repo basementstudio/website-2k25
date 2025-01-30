b608d30c9983e2e33f37f969f4c1f4a7608b14f4
git-chad <tobimocc@gmail.com>
2025-01-30 17:04:56 -0300

    temporal build fix

--- src/components/contact/contact-canvas.tsx
+++ src/components/contact/contact-canvas.tsx
@@ -1,27 +1,42 @@
 "use client"
 import { Canvas } from "@react-three/offscreen"
-import { lazy } from "react"
+import { lazy, useEffect, useState } from "react"
 
 import { useAssets } from "../assets-provider"
 
 const Fallback = lazy(() => import("./fallback"))
 
-const worker = new Worker(
-  new URL("@/workers/contact-worker.tsx", import.meta.url),
-  {
-    type: "module"
-  }
-)
-
 const ContactCanvas = () => {
   const { contactPhone, arcade } = useAssets()
+  const [worker, setWorker] = useState<Worker>()
+
+  useEffect(() => {
+    const newWorker = new Worker(
+      new URL("@/workers/contact-worker.tsx", import.meta.url),
+      {
+        type: "module"
+      }
+    )
+    setWorker(newWorker)
+
+    if (contactPhone && arcade) {
+      newWorker.postMessage({
+        type: "load-model",
+        modelUrl: contactPhone,
+        idleUrl: arcade.idleScreen
+      })
+    }
+
+    return () => {
+      newWorker.terminate()
+    }
+  }, [contactPhone, arcade])
+
   console.log("[ContactCanvas] model url", contactPhone)
 
-  worker.postMessage({
-    type: "load-model",
-    modelUrl: contactPhone,
-    idleUrl: arcade.idleScreen
-  })
+  if (!worker) {
+    return <Fallback />
+  }
 
   return (
     <Canvas
@@ -35,3 +50,41 @@ const ContactCanvas = () => {
 }
 
 export default ContactCanvas
+
+// "use client"
+// import { Canvas } from "@react-three/offscreen"
+// import { lazy } from "react"
+
+// import { useAssets } from "../assets-provider"
+
+// const Fallback = lazy(() => import("./fallback"))
+
+// const worker = new Worker(
+//   new URL("@/workers/contact-worker.tsx", import.meta.url),
+//   {
+//     type: "module"
+//   }
+// )
+
+// const ContactCanvas = () => {
+//   const { contactPhone, arcade } = useAssets()
+//   console.log("[ContactCanvas] model url", contactPhone)
+
+//   worker.postMessage({
+//     type: "load-model",
+//     modelUrl: contactPhone,
+//     idleUrl: arcade.idleScreen
+//   })
+
+//   return (
+//     <Canvas
+//       worker={worker}
+//       fallback={<Fallback />}
+//       shadows
+//       camera={{ position: [0, 0.082, 5.25], fov: 25 }}
+//       gl={{ antialias: false }}
+//     />
+//   )
+// }
+
+// export default ContactCanvas
