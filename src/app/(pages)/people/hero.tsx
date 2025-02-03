9162987524d32487070d7789688791d08ef2b1cf
Alan Daniel <stylesshjs@gmail.com>
2025-02-03 14:39:13 -0400

    content nits

--- src/app/(pages)/people/hero.tsx
+++ src/app/(pages)/people/hero.tsx
@@ -11,12 +11,12 @@ export const Hero = ({ data }: { data?: QueryType }) => {
       <h1 className="col-start-1 col-end-5 text-h1">
         {data?.pages.people._title}
       </h1>
-      <div className="col-start-5 col-end-9 max-w-[34.6875rem] text-h2">
+      <div className="col-start-5 col-end-9 max-w-[34.6875rem] text-h2 [&_p]:text-pretty">
         <RichText
           content={data?.pages?.people?.subheading1?.json?.content ?? []}
         />
       </div>
-      <div className="col-start-9 col-end-13 max-w-[34.6875rem] text-h2">
+      <div className="col-start-9 col-end-13 max-w-[34.6875rem] text-h2 [&_p]:text-pretty">
         <RichText
           content={data?.pages?.people?.subheading2?.json?.content ?? []}
         />
