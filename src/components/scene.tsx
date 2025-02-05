406c7b546f642c57a0d3b1da6d8540ec2e212746
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-02-05 15:12:19 -0300

    Remove unused environment map

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Environment } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Physics } from "@react-three/rapier"
 import { Leva } from "leva"
@@ -153,7 +152,6 @@ export const Scene = () => {
               <color attach="background" args={["#000"]} />
               <CameraController />
               <Inspectables />
-              <Environment preset="studio" />
               <Sparkles />
               <Physics paused={!isBasketball}>
                 <Map />
