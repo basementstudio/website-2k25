f0a110eeda6aa0ce97c3b914144a3e90b0b94b1d
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-02-05 12:21:22 -0300

    add fly controls

---
+++ src/components/camera/wasd-controls.tsx
@@ -0,0 +1,145 @@
+import { KeyboardControls, PerspectiveCamera } from "@react-three/drei"
+import { useKeyboardControls } from "@react-three/drei"
+import { Camera, useFrame } from "@react-three/fiber"
+import { ComponentRef, useEffect, useMemo, useRef, useState } from "react"
+import { Group, Vector2, Vector3 } from "three"
+import { useNavigationStore } from "../navigation-handler/navigation-store"
+
+enum Controls {
+  forward = "forward",
+  backward = "backward",
+  left = "left",
+  right = "right",
+  up = "up",
+  down = "down",
+  fast = "fast",
+  fov = "fov"
+}
+
+export function WasdControls() {
+  return (
+    <KeyboardControls
+      map={[
+        { name: Controls.forward, keys: ["ArrowUp", "w", "W"] },
+        { name: Controls.backward, keys: ["ArrowDown", "s", "S"] },
+        { name: Controls.left, keys: ["ArrowLeft", "a", "A"] },
+        { name: Controls.right, keys: ["ArrowRight", "d", "D"] },
+        { name: Controls.up, keys: ["e"] },
+        { name: Controls.down, keys: ["q"] },
+        { name: Controls.fast, keys: ["ShiftLeft", "ShiftRight"] },
+        { name: Controls.fov, keys: ["f"] }
+      ]}
+    >
+      <ControlsInner />
+    </KeyboardControls>
+  )
+}
+
+function ControlsInner() {
+  const mainCamera = useNavigationStore((s) => s.mainCamera)
+  const [, get] = useKeyboardControls<Controls>()
+
+  const fov = useKeyboardControls<Controls>((s) => s.fov)
+
+  const cameraRef = useRef<ComponentRef<typeof PerspectiveCamera>>(null)
+  const vectors = useMemo(
+    () => ({
+      moveTo: new Vector3()
+    }),
+    []
+  )
+
+  useFrame(() => {
+    if (!cameraRef.current) return
+    const { forward, backward, left, right, up, down, fast } = get()
+
+    const speed = fast ? 0.1 : 0.05
+
+    vectors.moveTo.set(
+      left ? -speed : right ? speed : 0,
+      0,
+      forward ? -speed : backward ? speed : 0
+    )
+
+    vectors.moveTo.applyEuler(cameraRef.current.rotation)
+
+    vectors.moveTo.y += up ? speed : down ? -speed : 0
+
+    cameraRef.current.position.add(vectors.moveTo)
+  })
+
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    let isPointerDown = false
+    let isContextMenu = false
+
+    const lastPointerPosition = new Vector2()
+
+    const onPointerDown = (event: PointerEvent) => {
+      isPointerDown = true
+      lastPointerPosition.set(event.clientX, event.clientY)
+    }
+
+    const onPointerMove = (event: PointerEvent) => {
+      if (!isPointerDown) return
+
+      const camera = cameraRef.current
+      if (!camera) return
+
+      // apply pointer moement
+      lastPointerPosition.x -= event.clientX
+      lastPointerPosition.y -= event.clientY
+      if (isContextMenu) {
+        // pan
+      } else {
+        // rotate
+        camera.rotation.order = "YXZ"
+        camera.rotation.x += lastPointerPosition.y * 0.002
+        camera.rotation.y += lastPointerPosition.x * 0.002
+
+        // camera.getWorldDirection(vectors.lookAt)
+      }
+      // reset pointer position
+      lastPointerPosition.set(event.clientX, event.clientY)
+    }
+    const onPointerUp = () => {
+      isPointerDown = false
+      isContextMenu = false
+    }
+    const onContextMenu = (event: MouseEvent) => {
+      lastPointerPosition.set(event.clientX, event.clientY)
+      isContextMenu = true
+    }
+
+    window.addEventListener("pointerdown", onPointerDown, { signal })
+    window.addEventListener("pointermove", onPointerMove, { signal })
+    window.addEventListener("pointerup", onPointerUp, { signal })
+    window.addEventListener("contextmenu", onContextMenu, { signal })
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
+  const [bigFov, setBigFov] = useState(false)
+
+  useEffect(() => {
+    if (!fov) return
+    setBigFov((current) => !current)
+  }, [fov])
+
+  return (
+    <>
+      <PerspectiveCamera
+        makeDefault
+        ref={cameraRef}
+        fov={bigFov ? 50 : 70}
+        rotation={mainCamera?.rotation}
+        position={mainCamera?.position}
+      />
+    </>
+  )
+}
