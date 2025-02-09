953ef34d161141b77910a6c8e763c5d4686f4768
Tomas Ferreras <hellotomasdev@gmail.com>
2025-02-09 15:26:01 -0300

    Tom/fix navigation routing (#91)
    * rem unsuded interface
    
    * feat: change mouse based on camera config changes
    
    * refactor: organize imports in navigation handler

--- src/components/navigation-handler/index.tsx
+++ src/components/navigation-handler/index.tsx
@@ -1,51 +1,66 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import { useCallback, useEffect, useMemo } from "react"
+import { useCallback, useEffect } from "react"
 
 import { useCurrentScene } from "@/hooks/use-current-scene"
+import { useHandleNavigation } from "@/hooks/use-handle-navigation"
+import { useKeyPress } from "@/hooks/use-key-press"
 
 import { useAssets } from "../assets-provider"
 import { useInspectable } from "../inspectables/context"
+import { IScene } from "./navigation.interface"
 import { useNavigationStore } from "./navigation-store"
-import { useKeyPress } from "@/hooks/use-key-press"
-import { useHandleNavigation } from "@/hooks/use-handle-navigation"
 
 export const NavigationHandler = () => {
   const pathname = usePathname()
   const { setSelected } = useInspectable()
 
+  const setScenes = useNavigationStore((state) => state.setScenes)
   const {
-    setScenes,
     isCanvasTabMode,
     setIsCanvasTabMode,
     currentScene,
     setCurrentScene,
-    currentTabIndex,
-    setCurrentTabIndex
+    currentTabIndex
   } = useNavigationStore()
-
-  const { scenes } = useAssets()
+  const scenes: IScene[] = useAssets().scenes
   const { handleNavigation } = useHandleNavigation()
   const scene = useCurrentScene()
 
   useEffect(() => setScenes(scenes), [scenes, setScenes])
 
-  const findCurrentScene = useMemo(() => {
-    if (!scenes.length) return null
-    return pathname === "/"
-      ? scenes.find((scene) => scene.name.toLowerCase() === "home")
-      : scenes.find((scene) => scene.name === pathname.split("/")[1])
-  }, [scenes, pathname])
+  const setPreviousTabIndex = useNavigationStore(
+    (state) => state.setPreviousTabIndex
+  )
+  const previousTabIndex = useNavigationStore((state) => state.previousTabIndex)
+  const setCurrentTabIndex = useNavigationStore(
+    (state) => state.setCurrentTabIndex
+  )
+
+  useEffect(() => setScenes(scenes), [scenes, setScenes])
 
   useEffect(() => {
     if (!scenes.length) return
+
     setSelected(null)
 
-    if (findCurrentScene && findCurrentScene.name !== scene) {
-      setCurrentScene(findCurrentScene)
-    }
-  }, [findCurrentScene, scene, scenes.length, setSelected, setCurrentScene])
+    const currentScene =
+      pathname === "/"
+        ? scenes.find((scene) => scene.name.toLowerCase() === "home")
+        : scenes.find((scene) => scene.name === pathname.split("/")[1])
+
+    if (currentScene && currentScene.name !== scene)
+      setCurrentScene(currentScene)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    scenes,
+    setSelected,
+    setPreviousTabIndex,
+    setCurrentTabIndex,
+    previousTabIndex
+  ])
 
   const handleKeyDown = useCallback(
     (e: KeyboardEvent) => {
@@ -56,15 +71,15 @@ export const NavigationHandler = () => {
       }
 
       e.preventDefault()
-      const maxIndex = currentScene.tabs.length - 1
-      let newIndex = e.shiftKey ? currentTabIndex - 1 : currentTabIndex + 1
-
-      if (newIndex < 0 || newIndex > maxIndex) {
-        setIsCanvasTabMode(false)
-        newIndex = newIndex < 0 ? 0 : maxIndex
-      }
-
+      const newIndex = e.shiftKey ? currentTabIndex - 1 : currentTabIndex + 1
       setCurrentTabIndex(newIndex)
+
+      if (
+        (e.shiftKey && currentTabIndex === 0) ||
+        (!e.shiftKey && currentTabIndex === currentScene.tabs.length - 1)
+      ) {
+        setIsCanvasTabMode(false)
+      }
     },
     [
       isCanvasTabMode,
@@ -75,35 +90,39 @@ export const NavigationHandler = () => {
     ]
   )
 
-  const handleEscape = useCallback(() => {
-    if (pathname === "/" || !scenes || window.scrollY > window.innerHeight)
-      return
-
-    const trimmedPathname = pathname.replace("/", "")
-    const tabIndex = scenes[0].tabs.findIndex(
-      (tab) => tab.tabName.toLowerCase() === trimmedPathname
-    )
-
-    if (
-      ["services", "blog", "people", "basketball", "lab", "showcase"].includes(
-        scene
-      )
-    ) {
-      handleNavigation("/")
-      setCurrentTabIndex(tabIndex)
-    }
-  }, [scene, handleNavigation, pathname, scenes, setCurrentTabIndex])
-
   useEffect(() => {
     if (pathname !== "/" && currentTabIndex !== -1) {
       setCurrentTabIndex(0)
     }
-  }, [setCurrentTabIndex, pathname, currentTabIndex])
+  }, [setCurrentTabIndex, pathname])
 
-  useEffect(() => setSelected(null), [scene, setSelected])
+  useEffect(() => setSelected(null), [scene])
 
   useKeyPress("Tab", handleKeyDown)
-  useKeyPress("Escape", handleEscape)
+  useKeyPress(
+    "Escape",
+    useCallback(() => {
+      if (pathname === "/" || !scenes || window.scrollY > window.innerHeight)
+        return
+
+      const trimmedPathname = pathname.replace("/", "")
+      const tabIndex = scenes[0].tabs.findIndex(
+        (tab) => tab.tabName.toLowerCase() === trimmedPathname
+      )
+
+      if (
+        scene === "services" ||
+        scene === "blog" ||
+        scene === "people" ||
+        scene === "basketball" ||
+        scene === "lab" ||
+        scene === "showcase"
+      ) {
+        handleNavigation("/")
+        setCurrentTabIndex(tabIndex)
+      }
+    }, [scene, handleNavigation, pathname, scenes])
+  )
 
   return null
 }
