93c8558809d8fa72efe9d3202158ce910780ec20
Alan Daniel <stylesshjs@gmail.com>
2025-02-19 14:36:44 -0400

    more updates

--- src/app/(pages)/showcase/[slug]/wrapper.tsx
+++ src/app/(pages)/showcase/[slug]/wrapper.tsx
@@ -6,7 +6,7 @@ import { QueryItemType } from "./query"
 
 export const ProjectWrapper = ({ entry }: { entry: QueryItemType }) => (
   <ProjectProvider>
-    <div className="grid-layout min-h-screen pt-2">
+    <div className="grid-layout min-h-screen pt-12">
       <ProjectGallery entry={entry} />
       <ProjectInfo entry={entry} />
     </div>
