25ba68e5fc0cb04e240f9d09e3b07988fe4dedf7
tomasferreras <tomas@basement.studio>
2025-02-24 18:50:29 +0000

    feat: increase hover underline size

--- src/components/arcade-screen/arcade-ui-components/arcade-labs-list.tsx
+++ src/components/arcade-screen/arcade-ui-components/arcade-labs-list.tsx
@@ -218,7 +218,7 @@ export const ArcadeLabsList = ({
                 </Text>
                 <Container
                   width={45}
-                  height={1}
+                  height={2}
                   positionTop={16}
                   positionType="absolute"
                   backgroundColor={
