516f1158dddcc65464526ba635204b6f97332bae
Alan Daniel <stylesshjs@gmail.com>
2025-02-28 16:31:15 -0400

    fix use client

--- src/components/layout/contact.tsx
+++ src/components/layout/contact.tsx
@@ -1,4 +1,6 @@
+"use client"
 import { Link } from "@/components/primitives/link"
+
 import { useContactStore } from "../contact/contact-store"
 
 export const Contact = () => {
@@ -6,26 +8,26 @@ export const Contact = () => {
 
   return (
     <div className="grid-layout">
-      <h2 className="text-mobile-h2 col-span-full text-brand-w1 lg:col-span-2 lg:text-h2">
+      <h2 className="col-span-full text-mobile-h2 text-brand-w1 lg:col-span-2 lg:text-h2">
         Contact:
       </h2>
 
       <div className="relative col-span-full grid h-40 grid-cols-4 grid-rows-[2rem,1rem] gap-2 !px-0 lg:col-span-10 lg:grid-cols-10">
         <Link
           href="mailto:hello@basement.studio"
-          className="text-mobile-h2 relative z-10 col-span-3 h-max w-max bg-brand-k text-brand-g1 lg:col-span-4 lg:text-h2"
+          className="relative z-10 col-span-3 h-max w-max bg-brand-k text-mobile-h2 text-brand-g1 lg:col-span-4 lg:text-h2"
         >
           (hello@basement.studio)
         </Link>
 
         <div className="col-span-full lg:col-span-6">
-          <p className="text-mobile-h2 relative z-10 h-max w-full bg-brand-k text-brand-w1 lg:w-max lg:text-h2">
+          <p className="relative z-10 h-max w-full bg-brand-k text-mobile-h2 text-brand-w1 lg:w-max lg:text-h2">
             Let&apos;s make an impact, together.
             <br />
           </p>
           <button
             onClick={() => setIsContactOpen(true)}
-            className="actionable text-mobile-h2 relative z-10 col-span-full h-max w-max bg-brand-k text-brand-w1 lg:col-start-6 lg:text-h2"
+            className="actionable relative z-10 col-span-full h-max w-max bg-brand-k text-mobile-h2 text-brand-w1 lg:col-start-6 lg:text-h2"
           >
             Get in touch
           </button>
