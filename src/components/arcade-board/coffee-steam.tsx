e810a61016685dbd55de0a3089d685e1638c74b7
git-chad <tobimocc@gmail.com>
2025-02-28 15:43:15 -0300

    fix steam

--- src/components/arcade-board/coffee-steam.tsx
+++ src/components/arcade-board/coffee-steam.tsx
@@ -3,7 +3,7 @@ import steamVert from "@/shaders/steam/steam.vert"
 import { useTexture } from "@react-three/drei"
 
 import perlin from "@/shaders/steam/perlin.jpg"
-import { useRef } from "react"
+import { useMemo } from "react"
 import { useFrame } from "@react-three/fiber"
 import { ShaderMaterial, RepeatWrapping } from "three"
 
@@ -12,30 +12,29 @@ const CoffeeSteam = () => {
   noise.wrapT = RepeatWrapping
   noise.wrapS = RepeatWrapping
 
-  const materialRef = useRef<ShaderMaterial>(null)
+  const material = useMemo(() => {
+    return new ShaderMaterial({
+      transparent: true,
+      side: 2,
+      depthWrite: false,
+      depthTest: false,
+      uniforms: {
+        uTime: { value: 0 },
+        uNoise: { value: noise }
+      },
+      fragmentShader: steamFrag,
+      vertexShader: steamVert
+    })
+  }, [noise])
 
   useFrame(({ clock }) => {
-    if (materialRef.current) {
-      materialRef.current.uniforms.uTime.value = clock.getElapsedTime()
-    }
+    material.uniforms.uTime.value = clock.getElapsedTime()
   })
 
   return (
     <mesh rotation={[0, 0, 0]} position={[2.57, 1.21, -13.87]}>
       <planeGeometry args={[0.065, 0.19, 16, 16]} />
-      <shaderMaterial
-        ref={materialRef}
-        transparent
-        side={2}
-        depthWrite={false}
-        depthTest={false}
-        uniforms={{
-          uTime: { value: 0 },
-          uNoise: { value: noise }
-        }}
-        fragmentShader={steamFrag}
-        vertexShader={steamVert}
-      />
+      <primitive object={material} attach="material" />
     </mesh>
   )
 }
