beae7d8b1e8640a84b623eb9b68e7c13fdfe75b9
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-02-28 16:04:47 -0300

    minor

--- src/app/(pages)/blog/[slug]/more.tsx
+++ src/app/(pages)/blog/[slug]/more.tsx
@@ -44,18 +44,20 @@ export default function More({
                   <Image
                     src={post.hero?.heroImage?.url ?? ""}
                     alt={post._title}
-                    fill
+                    width={272}
+                    height={120}
+                    className="h-full w-full object-cover"
                   />
                 )}
               </div>
 
               <div className="flex w-full justify-between gap-y-2">
-                <h3 className="line-clamp-2 w-full max-w-[70%] text-mobile-h3 text-brand-w2 lg:text-h3">
+                <h3 className="text-mobile-h3 line-clamp-2 w-full max-w-[70%] text-brand-w2 lg:text-h3">
                   {post._title}
                 </h3>
 
                 {post.date ? (
-                  <p className="flex-1 text-right text-mobile-p text-brand-g1 lg:flex-auto lg:text-p">
+                  <p className="text-mobile-p flex-1 text-right text-brand-g1 lg:flex-auto lg:text-p">
                     {formatDate(post.date ?? "")}
                   </p>
                 ) : null}
