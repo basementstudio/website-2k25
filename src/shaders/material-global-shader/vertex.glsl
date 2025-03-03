79503d06cd45b1275f9b777c5c10d167a602c2e5
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-03 12:48:42 -0300

    Allow inspectables to have dynamic light based on camera position

--- src/shaders/material-global-shader/vertex.glsl
+++ src/shaders/material-global-shader/vertex.glsl
@@ -4,7 +4,7 @@ varying vec2 vUv;
 varying vec3 vWorldPosition;
 varying vec3 vMvPosition;
 varying vec3 vNormal;
-
+varying vec3 vViewDirection;
 varying vec2 vUv2;
 
 void main() {
@@ -15,6 +15,9 @@ void main() {
 
   vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
   vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+
+  // Calculate view direction in view space
+  vViewDirection = normalize(-mvPosition.xyz);
 
   vMvPosition = mvPosition.xyz;
   vWorldPosition = worldPosition.xyz;
