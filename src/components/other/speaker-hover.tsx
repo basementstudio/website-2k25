a730fd635899ec312f125d82e27e1c47be722cb4
git-chad <tobimocc@gmail.com>
2025-03-11 11:57:38 -0300

    fetch

---
+++ src/components/other/speaker-hover.tsx
@@ -0,0 +1,11 @@
+export const SpeakerHover = () => {
+  return (
+    <mesh
+      position={[3.6949, 3.3415, -14.396]}
+      quaternion={[0.061, 0.029, 0.425, 0.903]}
+    >
+      <boxGeometry args={[0.28501, 0.21501, 0.47601]} />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  )
+}
