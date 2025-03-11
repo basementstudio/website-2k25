e45b43f12125264b257ad944dd25c3e450b4a2a9
git-chad <tobimocc@gmail.com>
2025-03-11 15:59:49 -0300

    add button on rightmost speaker

--- src/components/other/speaker-hover.tsx
+++ src/components/other/speaker-hover.tsx
@@ -10,7 +10,7 @@ export const SpeakerHover = () => {
 
   const handlePointerEnter = useCallback(() => {
     setHover(true)
-    setCursor("alias", "Play Next")
+    setCursor("pointer", "Play Next")
   }, [setCursor])
 
   const handlePointerLeave = useCallback(() => {
@@ -23,16 +23,30 @@ export const SpeakerHover = () => {
   }, [skipToNextTrack])
 
   return (
-    <mesh
-      visible={hover}
-      material={routingMaterial}
-      onPointerEnter={handlePointerEnter}
-      onPointerLeave={handlePointerLeave}
-      onClick={handleClick}
-      rotation={[Math.PI / 2, 0, Math.PI * 0.25]}
-      position={[3.6949, 3.52, -14.35]}
-    >
-      <boxGeometry args={[0.28, 0.24, 0.42]} />
-    </mesh>
+    <>
+      <mesh
+        visible={hover}
+        material={routingMaterial}
+        onPointerEnter={handlePointerEnter}
+        onPointerLeave={handlePointerLeave}
+        onClick={handleClick}
+        rotation={[Math.PI / 2, 0, Math.PI * 0.25]}
+        position={[3.6949, 3.52, -14.35]}
+      >
+        <boxGeometry args={[0.28, 0.24, 0.42]} />
+      </mesh>
+
+      <mesh
+        visible={hover}
+        material={routingMaterial}
+        onPointerEnter={handlePointerEnter}
+        onPointerLeave={handlePointerLeave}
+        onClick={handleClick}
+        rotation={[-Math.PI / 2, 0, -Math.PI * 0.25]}
+        position={[9.472, 3.52, -14.402]}
+      >
+        <boxGeometry args={[0.28, 0.24, 0.42]} />
+      </mesh>
+    </>
   )
 }
