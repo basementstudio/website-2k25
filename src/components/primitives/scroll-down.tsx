7c19af3059e64ce8b7ee5c3e437588484d1db79c
Jose Francisco Rago <ragojosefrancisco@gmail.com>
2025-03-10 21:39:13 -0300

    Refine text capitalization and wording across components (#183)

--- src/components/primitives/scroll-down.tsx
+++ src/components/primitives/scroll-down.tsx
@@ -14,7 +14,7 @@ export const ScrollDown = () => {
 
   return (
     <div className="fixed bottom-8 left-8 flex w-fit items-center gap-x-2 bg-brand-k px-1.5 py-0.5 text-p text-brand-w1">
-      Scroll Down <ArrowDownIcon className="size-2.5" />
+      Scroll to Explore <ArrowDownIcon className="size-2.5" />
     </div>
   )
 }
