7300ed867a8a030374f20c48840c0e8449f49cb2
valebearzotti <valebearzotti1@gmail.com>
2025-03-11 12:55:48 -0300

    ui grid fixes

--- src/app/(pages)/blog/layout.tsx
+++ src/app/(pages)/blog/layout.tsx
@@ -14,18 +14,16 @@ export default function BlogLayout({
       <div className="pb-25 relative flex flex-col gap-12 bg-brand-k lg:gap-20">
         <Hero />
         <Featured />
-        <div
-          className="col-span-full grid w-full grid-cols-12 gap-x-2 border-brand-w1/20 py-10 lg:col-start-1"
-          id="list"
-        >
-          <div className="col-span-full grid grid-cols-12 items-end gap-2 border-b border-brand-w1/20 pb-2">
-            <h2 className="col-span-full text-mobile-h3 text-brand-g1 lg:col-span-3 lg:col-start-5 lg:text-h3">
+
+        <section className="grid-layout pb-[35px]" id="list">
+          <div className="col-span-full grid grid-cols-12 gap-2">
+            <h2 className="col-span-full mt-auto text-mobile-h3 text-brand-g1 lg:col-span-3 lg:col-start-5 lg:text-h3">
               More News
             </h2>
             <Categories />
           </div>
           {children}
-        </div>
+        </section>
       </div>
     </>
   )
