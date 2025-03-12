2f5729e88809b9416a16e680c3625748aaed33bb
tomasferreras <tomas@basement.studio>
2025-03-12 14:36:56 -0300

    imrpvoe ui

--- src/components/contact/contact-screen.tsx
+++ src/components/contact/contact-screen.tsx
@@ -59,21 +59,70 @@ const ContactScreen = ({ worker }: ContactScreenProps) => {
           left: `${screenPosition.x * 100}%`,
           top: `${screenPosition.y * 100}%`,
           transform: `translate(-50%, -50%) `,
-          zIndex: 100,
-          pointerEvents: "none"
+          zIndex: 100
         }}
       >
         <div
-          className="relative flex h-[310px] w-[510px] bg-transparent p-2"
+          className="relative flex h-[300px] w-[510px] bg-transparent"
           style={{
             transform: `perspective(300px) rotateY(.5deg) scale(${scale})`,
             transformOrigin: "center center"
           }}
         >
-          <div className="h-full w-full border border-brand-o">
-            <span className="text-center text-[8px] font-bold text-white">
-              {screenPosition.x.toFixed(4)}, {screenPosition.y.toFixed(4)}
-            </span>
+          <div className="h-full w-full">
+            <div className="flex h-full w-full flex-col justify-between gap-7 text-[13px] text-brand-o">
+              <form className="relative flex h-full w-full flex-col justify-between border border-brand-o pb-4 pt-6 uppercase">
+                <fieldset className="absolute -top-[10px] left-[10px]">
+                  <legend className="bg-black px-1">fill in the form</legend>
+                </fieldset>
+                <div className="grid grid-cols-2 gap-2 px-4">
+                  <input
+                    type="text"
+                    placeholder="NAME"
+                    className="h-6 border-b border-dashed border-brand-o bg-transparent p-1 placeholder:text-brand-o/50"
+                  />
+                  <input
+                    type="text"
+                    placeholder="COMPANY"
+                    className="h-6 border-b border-dashed border-brand-o bg-transparent p-1 placeholder:text-brand-o/50"
+                  />
+                  <input
+                    type="email"
+                    placeholder="EMAIL"
+                    className="col-span-2 h-6 border-b border-dashed border-brand-o bg-transparent p-1 placeholder:text-brand-o/50"
+                  />
+                  <input
+                    type="number"
+                    placeholder="BUDGET (OPTIONAL)"
+                    className="col-span-2 h-6 border-b border-dashed border-brand-o bg-transparent p-1 placeholder:text-brand-o/50"
+                  />
+                  <textarea
+                    placeholder="MESSAGE"
+                    className="col-span-2 h-16 resize-none border-b border-dashed border-brand-o bg-transparent p-1 placeholder:text-brand-o/50"
+                  />
+                </div>
+                <div className="w-full px-4">
+                  <button className="h-8 w-full border border-brand-o/20 px-2 py-1 text-center">
+                    SUBMIT MESSAGE →
+                  </button>
+                </div>
+              </form>
+              <div className="flex items-center justify-between gap-2 text-[13px] uppercase text-brand-o">
+                <div className="flex items-center gap-2 underline underline-offset-4">
+                  <a href="" target="_blank">
+                    x (twitter)
+                  </a>
+                  <a href="" target="_blank">
+                    instagram
+                  </a>
+                  <a href="" target="_blank">
+                    github
+                  </a>
+                </div>
+
+                <p>hello@basement.studio</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
