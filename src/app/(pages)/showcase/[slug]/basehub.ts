13c623dc4a99cddaf9d24f2f96a46785db172afc
Valentina <64658886+valebearzotti@users.noreply.github.com>
2025-03-13 12:46:59 -0300

    [showcase] dynamic render images (#212)
    * Implement showcase functionality with new data fetching and UI components
    
    - Added `basehub.ts` for GraphQL fragments and data fetching for projects and categories.
    - Replaced `project-list.tsx` with `showcase-list` components for better organization.
    - Updated `hero.tsx` to fetch project count dynamically.
    - Refactored `grid.tsx` and `list.tsx` to utilize new project structure.
    - Enhanced filters and view mode selection in `filters.tsx` and `showcase-list/client.tsx`.
    - Removed obsolete files: `project-list.tsx`, `query.ts`.
    - Adjusted layout and navigation links for improved user experience.
    
    * image handling + wip basehub ts
    
    * refactor ImageWithVideoOverlay component to dynamically load MuxVideo and optimize video playback on hover. user intent timeout to reduce unnecessary video loading and improved cleanup for video resources.
    
    * remove user intent hover

---
+++ src/app/(pages)/showcase/[slug]/basehub.ts
@@ -0,0 +1,101 @@
+import { fragmentOn } from "basehub"
+
+import { client } from "@/service/basehub"
+
+const ImageFragment = fragmentOn("BlockImage", {
+  url: true,
+  width: true,
+  height: true,
+  alt: true,
+  blurDataURL: true
+})
+
+const VideoFragment = fragmentOn("BlockVideo", {
+  url: true,
+  width: true,
+  height: true
+})
+
+const ProjectFragment = fragmentOn("ProjectComponent", {
+  _title: true,
+  _slug: true,
+  client: {
+    _title: true
+  },
+  year: true,
+  categories: {
+    _slug: true,
+    _title: true
+  },
+  cover: ImageFragment,
+  coverVideo: VideoFragment,
+  icon: ImageFragment,
+  showcase: {
+    items: {
+      image: ImageFragment
+    },
+    __args: {
+      first: 6
+    }
+  }
+})
+
+export type Project = fragmentOn.infer<typeof ProjectFragment>
+
+export const fetchProject = async (slug: string) => {
+  const project = await client().query({
+    company: {
+      projects: {
+        projectList: {
+          item: {
+            ...ProjectFragment
+          },
+          __args: {
+            filter: {
+              _sys_slug: { eq: slug }
+            }
+          }
+        }
+      },
+      awards: {
+        awardList: {
+          items: {
+            title: true,
+            project: {
+              _id: true
+            }
+          },
+          __args: {
+            filter: {
+              project: {
+                _slug: { eq: slug }
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+
+  return {
+    project: project.company.projects.projectList.item,
+    awards: project.company.awards.awardList.items
+  }
+}
+
+export const fetchCategories = async () => {
+  const categories = await client().query({
+    company: {
+      projects: {
+        projectCategories: {
+          items: {
+            _title: true,
+            _slug: true
+          }
+        }
+      }
+    }
+  })
+
+  return categories.company.projects.projectCategories.items
+}
--- src/app/(pages)/showcase/hero.tsx
+++ src/app/(pages)/showcase/hero.tsx
@@ -1,13 +1,28 @@
-import { QueryType } from "./query"
+import { client } from "@/service/basehub"
 
-export const Hero = ({ data }: { data: QueryType }) => (
-  <section className="grid-layout">
-    <h1 className="col-span-3 text-mobile-h1 text-brand-w2 lg:col-start-1 lg:col-end-7 lg:text-h1">
-      Showcase
-    </h1>
+const fetchProjectsLength = async () => {
+  const res = await client().query({
+    pages: {
+      showcase: {
+        projectList: { _meta: { totalCount: true } }
+      }
+    }
+  })
 
-    <div className="col-span-1 text-mobile-h1 text-brand-g1 lg:col-start-7 lg:col-end-12 lg:text-h1">
-      {data.pages.showcase.projectList.items.length}
-    </div>
-  </section>
-)
+  return res.pages.showcase.projectList._meta.totalCount
+}
+
+export async function Hero() {
+  const length = await fetchProjectsLength()
+
+  return (
+    <section className="grid-layout">
+      <h1 className="col-span-3 text-mobile-h1 text-brand-w2 lg:col-start-1 lg:col-end-7 lg:text-h1">
+        Showcase
+      </h1>
+      <div className="col-span-1 text-mobile-h1 text-brand-g1 lg:col-start-7 lg:col-end-12 lg:text-h1">
+        {length}
+      </div>
+    </section>
+  )
+}
