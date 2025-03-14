aa1965d102e6958094bb1c9ba0f775507d180748
tomasferreras <tomas@basement.studio>
2025-03-14 16:44:35 -0300

    feat: add contact form validations

---
+++ src/app/(pages)/contact/contact-status-handler.tsx
@@ -0,0 +1,92 @@
+import React from "react"
+import { FieldErrors } from "react-hook-form"
+
+export interface ContactFormValues {
+  name: string
+  email: string
+  message: string
+}
+
+export const ContactStatusHandler = ({
+  isSubmitted,
+  errors,
+  customError,
+  isSubmitting
+}: {
+  isSubmitted: boolean
+  errors: FieldErrors<ContactFormValues>
+  customError: string | null
+  isSubmitting: boolean
+}) => {
+  return (
+    <>
+      {isSubmitted && (
+        <div className="flex items-center gap-2">
+          <p className="text-h4 text-[#00FF9B]">Form submitted</p>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_5476_57669)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M20 20.0005H0V0.000488281L20 0.00049918V20.0005ZM14.7696 7.89509L15.4767 7.18799L14.0625 5.77377L13.3554 6.48088L8.125 11.7113L6.64461 10.2309L5.93751 9.52378L4.52329 10.938L5.23039 11.6451L7.41789 13.8326C7.60543 14.0202 7.85978 14.1255 8.125 14.1255C8.39022 14.1255 8.64457 14.0202 8.83211 13.8326L14.7696 7.89509Z"
+                fill="#00FF9B"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_5476_57669">
+                <rect
+                  width="20"
+                  height="20"
+                  fill="white"
+                  transform="translate(0 0.000488281)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+      )}
+      {(errors.email || errors.name || errors.message || customError) && (
+        <div className="flex items-center gap-2">
+          <p className="text-h4 text-[#F32D2D]">
+            {errors.name?.message ||
+              errors.email?.message ||
+              errors.message?.message ||
+              customError}
+          </p>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_5476_58668)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M5.85786 0.000427246H14.1421L20 5.85829V14.1426L14.1421 20.0004H5.85786L0 14.1426V5.85829L5.85786 0.000427246ZM11 4.62543V5.62543V10.0004V11.0004H9V10.0004V5.62543V4.62543H11ZM10 15.0004C10.6904 15.0004 11.25 14.4408 11.25 13.7504C11.25 13.0601 10.6904 12.5004 10 12.5004C9.30964 12.5004 8.75 13.0601 8.75 13.7504C8.75 14.4408 9.30964 15.0004 10 15.0004Z"
+                fill="#F32D2D"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_5476_58668">
+                <rect
+                  width="20"
+                  height="20"
+                  fill="white"
+                  transform="translate(0 0.000427246)"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+      )}
+    </>
+  )
+}
--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,15 +1,15 @@
 import { ContactForm } from "./contact-form"
 const Contact = () => {
   return (
-    <div className="grid-layout h-[calc(100vh-24px)] w-full grid-rows-2 pb-6 text-brand-w1">
-      <div className="col-start-7 col-end-13 row-start-1 row-end-3 flex h-full flex-col gap-6">
-        <ContactForm />
-      </div>
-      <h1 className="col-start-1 col-end-5 row-start-1 row-end-2 h-fit text-[98px] font-semibold">
+    <div className="grid-layout h-[calc(100vh-24px)] w-full grid-cols-4 grid-rows-[auto_1fr] gap-y-6 pb-6 text-brand-w1 xl:grid-cols-12 xl:grid-rows-2">
+      <h1 className="text-f-h0-mobile xl:text-f-h0 col-span-4 font-semibold xl:col-start-1 xl:col-end-6 xl:row-start-1 xl:row-end-2 xl:h-fit xl:text-[98px]">
         Contact us
       </h1>
-      <div className="col-start-1 col-end-4 row-start-2 row-end-3 flex flex-col justify-end gap-6">
-        <div className="flex w-full flex-col text-[56px] font-semibold leading-none">
+      <div className="col-span-4 row-span-1 flex h-full flex-col gap-6 xl:col-start-7 xl:col-end-13 xl:row-start-1 xl:row-end-3">
+        <ContactForm />
+      </div>
+      <div className="col-span-4 row-span-1 flex flex-col justify-end gap-6 xl:col-start-1 xl:col-end-4 xl:row-start-2 xl:row-end-3">
+        <div className="flex w-full flex-col text-[54px] font-semibold leading-none xl:text-[56px]">
           <a
             href="mailto:hello@basement.studio"
             className="text-wrap break-words hover:underline focus:outline-none focus:ring-2 focus:ring-brand-w1"
@@ -21,7 +21,7 @@ const Contact = () => {
             studio
           </a>
         </div>
-        <div className="flex items-center gap-2 text-h3">
+        <div className="flex flex-wrap items-center gap-2 text-mobile-h3 xl:text-h3">
           <a
             href="https://twitter.com/basement"
             className="hover:underline focus:outline-none focus:ring-2 focus:ring-brand-w1"
