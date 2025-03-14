b6be6a02cf2fecb73f323634180d6ac18df1e11c
tomasferreras <tomas@basement.studio>
2025-03-14 10:32:38 -0300

    add marquee

--- src/app/(pages)/contact/contact-marquee.tsx
+++ src/app/(pages)/contact/contact-marquee.tsx
@@ -1,3 +1,40 @@
+import { Marquee } from "@/components/primitives/marquee"
+
 export const ContactMarquee = () => {
-  return <div className="h-screen w-full"></div>
+  return (
+    <section className="mb-4" role="region" aria-label="Contact us information">
+      <Marquee aria-hidden="true" className="mb-4">
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          CONTACT US
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          /
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          CONTACT US
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          /
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          CONTACT US
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          /
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          CONTACT US
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          /
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          CONTACT US
+        </span>
+        <span className="mr-8 text-nowrap text-h1 font-bold uppercase text-brand-o">
+          /
+        </span>
+      </Marquee>
+    </section>
+  )
 }
