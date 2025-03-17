0b1593083c108345b97fa67b83546f593ede8e7c
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-17 14:46:39 -0300

    Pause main rendering while loading || is scrolled

--- src/components/layout/content-wrapper.tsx
+++ src/components/layout/content-wrapper.tsx
@@ -13,7 +13,7 @@ const Scene = dynamic(
     loading: () => null
   }
 )
-import { useGlobalFrameLoop } from "@/hooks/use-pausable-time"
+
 import { cn } from "@/utils/cn"
 
 import { useAppLoadingStore } from "../loading/app-loading-handler"
@@ -24,8 +24,6 @@ const BLACKLISTED_PATHS = [
   /^\/showcase\/[^\/]+$/,
   /^\/post\/[^\/]+$/,
   /^\/project\/[^\/]+$/
-  // temp
-  // /^\/showcase$/
 ]
 
 export const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
