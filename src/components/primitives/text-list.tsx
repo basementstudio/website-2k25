97f4f72b622dcd0d276f1a2e5dfe830c7f9df9b4
Nazareno Oviedo <nazareno@basement.studio>
2025-03-18 14:36:33 -0300

    Footer (Jose Requests) (#230)

--- src/components/primitives/text-list.tsx
+++ src/components/primitives/text-list.tsx
@@ -5,17 +5,19 @@ import { cn } from "@/utils/cn"
 export const TextList = ({
   value,
   className,
-  showComma = true
+  showComma = true,
+  hasWrapper = false
 }: {
   value: ReactNode[]
   className?: string
   showComma?: boolean
+  hasWrapper?: boolean
 }) => {
   return (
     <p className={cn("inline-flex flex-wrap gap-x-1 text-p", className)}>
       {value.map((v, i) => (
         <Fragment key={`text-item-${i}-${v}`}>
-          {v}
+          {hasWrapper ? <span>{v}</span> : v}
           {showComma && i < value.length - 1 && (
             <span className="text-brand-g1">, </span>
           )}
