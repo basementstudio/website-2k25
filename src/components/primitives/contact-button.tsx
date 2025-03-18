9138557242bd48c2d30afbb74915285050b13b7b
tomasferreras <tomas@basement.studio>
2025-03-18 18:20:27 +0000

    Implement webgl detection in ContactButton and add useWebgl hook

--- src/components/primitives/contact-button.tsx
+++ src/components/primitives/contact-button.tsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/navigation"
 import { useContactStore } from "../contact/contact-store"
+import { useWebgl } from "@/hooks/use-webgl"
 
 export const ContactButton = ({ children }: { children: React.ReactNode }) => {
   const setIsContactOpen = useContactStore((state) => state.setIsContactOpen)
   const isContactOpen = useContactStore((state) => state.isContactOpen)
   const router = useRouter()
+  const webglEnabled = useWebgl()
 
   const handleClick = () => {
-    // detect if user has webgl enabled
-    if (window.WebGLRenderingContext) {
+    if (webglEnabled) {
       setIsContactOpen(!isContactOpen)
     } else {
       router.push("/contact")
