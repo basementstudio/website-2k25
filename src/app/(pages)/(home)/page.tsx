8ae34e1fd3a29af83d8fca925a994bdd4ae42519
valebearzotti <valebearzotti1@gmail.com>
2025-03-18 15:04:22 -0300

    remove pump on homepage

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,31 +1,25 @@
-import { Pump } from "basehub/react-pump"
-
 import { Contact } from "@/components/layout/contact"
 
+import { fetchHomepage } from "./basehub"
 import { Brands } from "./brands"
 import { BrandsMobile } from "./brands-mobile"
 import { Capabilities } from "./capabilities"
 import { FeaturedProjects } from "./featured-projects"
 import { Intro } from "./intro"
-import { query } from "./query"
 
-const Homepage = () => (
-  <Pump queries={[query]}>
-    {async ([data]) => {
-      "use server"
+const Homepage = async () => {
+  const data = await fetchHomepage()
 
-      return (
-        <div className="flex flex-col gap-18 lg:gap-44">
-          <Intro data={data} />
-          <Brands data={data} />
-          <BrandsMobile />
-          <FeaturedProjects data={data} />
-          <Capabilities data={data} />
-          <Contact />
-        </div>
-      )
-    }}
-  </Pump>
-)
+  return (
+    <div className="flex flex-col gap-18 lg:gap-44">
+      <Intro data={data} />
+      <Brands data={data} />
+      <BrandsMobile />
+      <FeaturedProjects data={data} />
+      <Capabilities data={data} />
+      <Contact />
+    </div>
+  )
+}
 
 export default Homepage
