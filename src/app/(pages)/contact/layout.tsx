930b21db73c5ff91aa1f20779818bed62d31cfd9
Tomas Ferreras <hellotomasdev@gmail.com>
2025-03-18 11:16:05 -0300

    UI contact (#228)

---
+++ src/app/(pages)/contact/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/layout/footer"
+import { Navbar } from "@/components/layout/navbar"
+
+const ContactLayout = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <Navbar />
+    <main className="relative flex flex-col bg-brand-k pb-24 after:absolute after:-top-px after:z-10 after:h-px after:w-full after:bg-brand-w1/10">
+      {children}
+    </main>
+    <Footer />
+  </>
+)
+
+export default ContactLayout
