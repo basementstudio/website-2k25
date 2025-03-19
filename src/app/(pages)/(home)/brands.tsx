0be083e602a526653b23b0f3b55057f4a13acb47
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-03-19 12:08:48 -0300

    upd

--- src/app/(pages)/(home)/brands.tsx
+++ src/app/(pages)/(home)/brands.tsx
@@ -11,18 +11,6 @@ export type Brand = {
   website: Scalars["String"] | null
 }
 
-const getBrandsMobile = (brands: Brand[]) => {
-  // Check if brands lenght is multiple of 6
-  if (brands.length % 6 !== 0) {
-    // if not, remove exceeded brands
-    const brandsToRemove = brands.length % 6
-    const brandsToKeep = brands.length - brandsToRemove
-    return [brands.slice(0, brandsToKeep / 2), brands.slice(brandsToKeep / 2)]
-  }
-
-  return [brands.slice(0, brands.length / 2), brands.slice(brands.length / 2)]
-}
-
 export const Brands = ({ data }: { data: QueryType }) => {
   const brands =
     data.company.clients?.clientList.items.filter((c) => c.logo) ?? []
@@ -30,7 +18,12 @@ export const Brands = ({ data }: { data: QueryType }) => {
   return (
     <>
       <BrandsDesktop brands={brands} />
-      <BrandsMobile brandsMobile={getBrandsMobile(brands)} />
+      <BrandsMobile
+        brandsMobile={[
+          brands.slice(0, brands.length / 2),
+          brands.slice(brands.length / 2)
+        ]}
+      />
     </>
   )
 }
