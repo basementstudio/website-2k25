c944b2cc4b03d97a65c0e6973e5f248e85305f79
valebearzotti <valebearzotti1@gmail.com>
2025-03-19 11:22:45 -0300

    test remove fetch and contact from homepge

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next"
 
-import { Contact } from "@/components/layout/contact"
-
-import { fetchHomepage } from "./basehub"
-import { Brands } from "./brands"
 import { BrandsMobile } from "./brands-mobile"
-import { Capabilities } from "./capabilities"
-import { FeaturedProjects } from "./featured-projects"
-import { Intro } from "./intro"
 
 export const metadata: Metadata = {
   title: "We make cool shit that performs.",
@@ -16,16 +9,9 @@ export const metadata: Metadata = {
 }
 
 const Homepage = async () => {
-  const data = await fetchHomepage()
-
   return (
     <div className="flex flex-col gap-18 lg:gap-44">
-      <Intro data={data} />
-      <Brands data={data} />
       <BrandsMobile />
-      <FeaturedProjects data={data} />
-      <Capabilities data={data} />
-      <Contact />
     </div>
   )
 }
