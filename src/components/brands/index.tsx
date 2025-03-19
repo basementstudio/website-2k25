335af871b3af9cc652fb6f9721ca75bf352575b6
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-03-19 12:18:24 -0300

    nits

--- src/components/brands/index.tsx
+++ src/components/brands/index.tsx
@@ -6,6 +6,7 @@ import { memo, useMemo, useState } from "react"
 import { Brand } from "@/app/(pages)/(home)/brands"
 import { ExternalLinkIcon } from "@/components/icons/icons"
 import useDebounceValue from "@/hooks/use-debounce-value"
+import { cn } from "@/utils/cn"
 
 const DEBOUNCE_DELAY = 50
 
@@ -70,9 +71,6 @@ export const BrandsDesktop = ({ brands }: { brands: Brand[] }) => {
   return (
     <section className="lg:grid-layout hidden !gap-y-4">
       <div className="grid-layout col-span-full !px-0">
-        <p className="col-start-1 text-mobile-h3 text-brand-g1 lg:text-h3">
-          (1)
-        </p>
         <h3 className="col-span-full text-mobile-h3 text-brand-g1 lg:col-start-2 lg:text-h3 2xl:col-start-3">
           Trusted by <AnimatedTitle brandName={hoveredBrandName ?? ""} />
         </h3>
@@ -100,7 +98,12 @@ export const BrandsDesktop = ({ brands }: { brands: Brand[] }) => {
               title={brand._title}
             >
               <div className="relative h-full after:pointer-events-none after:absolute after:inset-0 after:border after:border-brand-w1/20">
-                <div className="with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+                <div
+                  className={cn(
+                    "with-diagonal-lines pointer-events-none !absolute -bottom-px -top-px left-0 right-0 opacity-0 transition-opacity duration-300",
+                    hoveredBrandId === brand._id && "opacity-100"
+                  )}
+                />
                 <SVGLogo svg={brand.logo} />
               </div>
             </motion.a>
