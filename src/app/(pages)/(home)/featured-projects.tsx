ef14ee64b59e9a1271bc55795ab9277091777b26
Nacho Mandagaran <33981565+ignmandagaran@users.noreply.github.com>
2025-03-20 11:16:06 -0300

    Featured projects hover (#249)
    * refactor custom cursor handling
    
    * add custom cursor on home featured projects images
    
    * fix brands logos width
    
    * Add color-scheme dark
    
    * Add TODO comment
    
    * change custom cursor position logic and update cursor type handling in useCursor hook
    
    * improve cursor positioning
    
    ---------
    
    Co-authored-by: Tomas Ferreras <hellotomasdev@gmail.com>

--- src/app/(pages)/(home)/featured-projects.tsx
+++ src/app/(pages)/(home)/featured-projects.tsx
@@ -1,10 +1,10 @@
 import { Arrow } from "@/components/primitives/icons/arrow"
-import { ImageWithVideoOverlay } from "@/components/primitives/image-with-video-overlay"
 import { Link } from "@/components/primitives/link"
 import { TextList } from "@/components/primitives/text-list"
 import { cn } from "@/utils/cn"
 
 import type { QueryType } from "./query"
+import { ShowcaseImage } from "./showcase-image"
 
 export const FeaturedProjects = ({ data }: { data: QueryType }) => {
   const p = data.pages.homepage.featuredProjects.projectList.items
@@ -54,17 +54,7 @@ const ProjectItem = ({ project }: ProjectItemProps) => (
     )}
   >
     <div className="relative col-span-full after:pointer-events-none after:absolute after:inset-0 after:border after:border-brand-w1/20 lg:col-span-7">
-      {project.cover && (
-        <Link href={`/showcase/${project.project?._slug}`}>
-          <div className="with-dots relative h-full w-full">
-            <ImageWithVideoOverlay
-              image={project.cover}
-              video={project.coverVideo || project.project?.coverVideo}
-              className="aspect-video"
-            />
-          </div>
-        </Link>
-      )}
+      <ShowcaseImage project={project} />
     </div>
     <div className="col-span-2 flex flex-col justify-between gap-y-4 pr-2 md:col-span-3 md:pr-12">
       <p className="text-f-h4-mobile lg:text-f-h4 text-brand-w2">
