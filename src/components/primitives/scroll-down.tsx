cb37b238d23a051f577a4cab14c9247ac3d8bc3b
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-03-20 12:39:12 -0300

    fix scroll to explore blink on page load

--- src/components/primitives/scroll-down.tsx
+++ src/components/primitives/scroll-down.tsx
@@ -1,16 +1,18 @@
 import { useCurrentScene } from "@/hooks/use-current-scene"
 
 import { ArrowDownIcon } from "../icons/icons"
+import { useAppLoadingStore } from "../loading/app-loading-handler"
 
 export const ScrollDown = () => {
   const scene = useCurrentScene()
+  const canRunMainApp = useAppLoadingStore((state) => state.canRunMainApp)
   const is404 = scene === "404"
   const isArcade = scene === "lab"
   const isBasketball = scene === "basketball"
 
   const shouldIgnore = is404 || isArcade || isBasketball
 
-  if (shouldIgnore) return null
+  if (shouldIgnore || !canRunMainApp) return null
 
   return (
     <div className="fixed bottom-8 left-1/2 flex w-fit -translate-x-1/2 items-center gap-x-2 bg-brand-k px-1.5 py-0.5 text-p text-brand-w1">
