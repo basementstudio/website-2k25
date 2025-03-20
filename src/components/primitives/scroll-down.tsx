7c1dcc44c6b4143e9bfde7393d86a206f4303469
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-03-20 12:44:55 -0300

    scroll to main content with scroll to explore link

--- src/components/primitives/scroll-down.tsx
+++ src/components/primitives/scroll-down.tsx
@@ -15,8 +15,11 @@ export const ScrollDown = () => {
   if (shouldIgnore || !canRunMainApp) return null
 
   return (
-    <div className="fixed bottom-8 left-1/2 flex w-fit -translate-x-1/2 items-center gap-x-2 bg-brand-k px-1.5 py-0.5 text-p text-brand-w1">
+    <a
+      href="#main-content"
+      className="fixed bottom-8 left-1/2 flex w-fit -translate-x-1/2 items-center gap-x-2 bg-brand-k px-1.5 py-0.5 text-p text-brand-w1"
+    >
       Scroll to Explore <ArrowDownIcon className="size-2.5" />
-    </div>
+    </a>
   )
 }
