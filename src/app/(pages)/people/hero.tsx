43d93befa0636a6467805e1fa2b82289e5e30246
Tobi Moccagatta <tobimocc@gmail.com>
2025-03-20 10:58:03 -0300

    services fixes (#251)

--- src/app/(pages)/people/hero.tsx
+++ src/app/(pages)/people/hero.tsx
@@ -8,7 +8,7 @@ export const Hero = ({ data }: { data?: QueryType }) => {
       className="grid-layout mb-18 !gap-y-6 text-brand-w2 lg:mb-44 lg:!gap-y-2"
       aria-labelledby="careers-heading"
     >
-      <h1 className="text-f-h0-mobile lg:text-f-h0 col-span-full lg:col-start-1 lg:col-end-5">
+      <h1 className="col-span-full !text-[3.5rem] text-f-h0-mobile lg:col-start-1 lg:col-end-5 lg:text-f-h0">
         {data?.pages.people._title}
       </h1>
       <div className="col-span-full flex flex-col gap-4 lg:col-start-5 lg:col-end-9 [&_p]:!text-pretty [&_p]:text-mobile-h2 lg:[&_p]:text-h2">
