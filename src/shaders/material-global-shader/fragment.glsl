482f23ccc85419cad6649189152a81a6d236a2d4
Nazareno Oviedo <nazarenooviedo@gmail.com>
2025-03-19 23:12:43 -0300

    back

--- src/shaders/material-global-shader/fragment.glsl
+++ src/shaders/material-global-shader/fragment.glsl
@@ -7,15 +7,21 @@ varying vec3 vMvPosition;
 varying vec3 vNormal;
 varying vec3 vViewDirection;
 
+// base color
+uniform vec3 uColor;
 uniform vec3 baseColor;
 uniform sampler2D map;
 uniform mat3 mapMatrix;
 uniform vec2 mapRepeat;
 
+// other
 uniform float uTime;
+
+// lightmap
 uniform sampler2D lightMap;
 uniform float lightMapIntensity;
 
+// lights
 #ifdef LIGHT
 uniform vec3 lightDirection;
 #endif
@@ -24,13 +30,19 @@ uniform vec3 lightDirection;
 uniform vec3 backLightDirection;
 #endif
 
+// aomap
 uniform sampler2D aoMap;
 uniform float aoMapIntensity;
 
+uniform float noiseFactor;
+uniform bool uReverse;
+
+// transparency
 uniform float opacity;
 uniform sampler2D alphaMap;
 uniform mat3 alphaMapTransform;
 
+// emissive
 #ifdef USE_EMISSIVE
 uniform vec3 emissive;
 uniform float emissiveIntensity;
@@ -41,65 +53,86 @@ uniform sampler2D emissiveMap;
 uniform float emissiveIntensity;
 #endif
 
+// Fog
 #ifdef FOG
 uniform vec3 fogColor;
 uniform float fogDensity;
 uniform float fogDepth;
 #endif
 
+// Matcap
 #ifdef MATCAP
 uniform sampler2D matcap;
 uniform bool glassMatcap;
 #endif
 
+// Glass
 #ifdef GLASS
 uniform sampler2D glassReflex;
 #endif
 
+// Godray
 #ifdef GODRAY
 uniform float uGodrayOpacity;
 uniform float uGodrayDensity;
 #endif
 
+// Daylight
 #ifdef DAYLIGHT
 uniform bool daylight;
 #endif
 
+// Inspectable
 uniform bool inspectingEnabled;
 uniform float inspectingFactor;
 uniform float fadeFactor;
 
+// Lamp
 uniform sampler2D lampLightmap;
 uniform bool lightLampEnabled;
 
-const float RECIPROCAL_PI = 0.31830988618;
+const float RECIPROCAL_PI = 1.0 / 3.14159265359;
 #pragma glslify: valueRemap = require('../utils/value-remap.glsl')
 #pragma glslify: basicLight = require('../utils/basic-light.glsl')
 
 void main() {
-  vec3 normal = normalize(vNormal);
-  vec3 viewDir = normalize(vViewDirection);
+  vec2 shiftedFragCoord = gl_FragCoord.xy + vec2(1.0);
+  vec2 checkerPos = floor(shiftedFragCoord * 0.5);
+  float pattern = mod(checkerPos.x + checkerPos.y, 2.0);
 
+  // Render as solid color
+  vec4 mapSample = vec4(1.0);
+
+  // TODO: implement map matrix instead of mapRepeat
+  #ifdef USE_MAP
   vec2 mapUv = (mapMatrix * vec3(vUv, 1.0)).xy;
-
-  #ifdef CLOUDS
-  mapUv.x -= uTime * 0.004;
+  mapSample = texture2D(map, mapUv * mapRepeat);
   #endif
 
-  vec4 mapSample = texture2D(map, mapUv * mapRepeat);
-  vec3 irradiance = baseColor * mapSample.rgb;
+  // TODO: use map matrix to shift or move to a different shader to add lightning
+  #ifdef CLOUDS
+  mapSample = texture2D(map, vec2(vUv.x - uTime * 0.004, vUv.y));
+  #endif
 
-  vec3 lightMapSample = lightLampEnabled
-    ? texture2D(lampLightmap, vUv2).rgb
-    : texture2D(lightMap, vUv2).rgb;
+  // Combine texture and base color
+  vec3 color = baseColor * mapSample.rgb;
 
-  float fadeMult =
-    inspectingEnabled && inspectingFactor <= 0.0
-      ? 1.0 - fadeFactor
-      : 1.0;
+  vec3 lightMapSample = vec3(0.0);
+
+  if (lightLampEnabled) {
+    lightMapSample = texture2D(lampLightmap, vUv2).rgb;
+  } else {
+    lightMapSample = texture2D(lightMap, vUv2).rgb;
+  }
+
+  vec3 irradiance = color;
 
   #ifdef USE_EMISSIVE
-  irradiance += emissive * emissiveIntensity * fadeMult;
+  float ei = emissiveIntensity;
+  if (inspectingEnabled && !(inspectingFactor > 0.0)) {
+    ei *= 1.0 - fadeFactor;
+  }
+  irradiance += emissive * ei;
   #endif
 
   #ifdef USE_EMISSIVEMAP
@@ -122,72 +155,84 @@ void main() {
     // Rim light
     vec3 rimLightDir = normalize(-vViewDirection + vec3(0.0, 0.5, 0.0));
     lf *= basicLight(vNormal, rimLightDir, 3.0);
+
+    #ifdef MATCAP
+    vec3 x = normalize(vec3(-vViewDirection.z, 0.0, vViewDirection.x));
+    vec3 y = cross(vViewDirection, x);
+    vec2 muv =
+      vec2(dot(x, normalize(vNormal)), dot(y, normalize(vNormal))) * 0.495 +
+      0.5;
+    lf *= texture2D(matcap, muv).rgb;
+    #endif
   }
 
   #ifndef VIDEO
-  if (lightMapIntensity > 0.0) irradiance *= lightMapSample * lightMapIntensity;
+  if (lightMapIntensity > 0.0) {
+    irradiance *= lightMapSample * lightMapIntensity;
+  }
   #endif
 
-  if (aoMapIntensity > 0.0)
-    irradiance *= (texture2D(aoMap, vUv2).r - 1.0) * aoMapIntensity + 1.0;
+  if (aoMapIntensity > 0.0) {
+    float ambientOcclusion =
+      (texture2D(aoMap, vUv2).r - 1.0) * aoMapIntensity + 1.0;
+
+    irradiance *= ambientOcclusion;
+  }
 
   irradiance = mix(irradiance, lf, inspectingFactor);
 
-  float opacityResult = opacity;
+  float opacityResult = 1.0;
+  opacityResult *= opacity;
+
   #ifdef IS_TRANSPARENT
-  opacityResult *= mapSample.a;
+  float mapAlpha = mapSample.a;
+  opacityResult *= mapAlpha;
   #endif
 
   #ifdef USE_ALPHA_MAP
-  opacityResult *= texture2D(
-    alphaMap,
-    (alphaMapTransform * vec3(vUv, 1.0)).xy
-  ).r;
+  vec2 alphaMapUv = (alphaMapTransform * vec3(vUv, 1.0)).xy;
+  float alpha = texture2D(alphaMap, alphaMapUv).r;
+  opacityResult *= alpha;
   #endif
 
-  if (opacityResult <= 0.0) discard;
+  if (opacityResult <= 0.0) {
+    discard;
+  }
 
   #ifdef LIGHT
-  float lightFactor = clamp(
-    valueRemap(dot(lightDirection, normal), 0.2, 1.0, 0.1, 1.0),
-    0.0,
-    1.0
-  );
-  lightFactor *= lightFactor;
+  float lightFactor = dot(lightDirection, normalize(vNormal));
+  lightFactor = valueRemap(lightFactor, 0.2, 1.0, 0.1, 1.0);
+  lightFactor = clamp(lightFactor, 0.0, 1.0);
+  lightFactor = pow(lightFactor, 2.0);
 
   #ifdef BASKETBALL
-  float backLightFactor = clamp(
-    valueRemap(dot(backLightDirection, normal), 0.05, 1.0, 0.1, 1.0),
-    0.0,
-    1.0
-  );
-  backLightFactor *= backLightFactor;
-  lightFactor = max(lightFactor * 8.0, backLightFactor * 6.0);
+  float backLightFactor = dot(backLightDirection, normalize(vNormal));
+  backLightFactor = valueRemap(backLightFactor, 0.05, 1.0, 0.1, 1.0);
+  backLightFactor = clamp(backLightFactor, 0.0, 1.0);
+  backLightFactor = pow(backLightFactor, 2.0);
+
+  lightFactor *= 8.0;
+  backLightFactor *= 4.0;
+
+  lightFactor = max(lightFactor, backLightFactor * 1.5);
   #else
   lightFactor *= 3.0;
   #endif
 
-  irradiance *= lightFactor + 1.0;
+  lightFactor += 1.0;
+  irradiance *= lightFactor;
   #endif
 
   gl_FragColor = vec4(irradiance, opacityResult);
 
-  #if defined(GLASS) ||                                                          \
-    defined(GODRAY) ||                                                         \
-    defined(MATCAP) && defined(glassMatcap)
-  float pattern = mod(
-    floor((gl_FragCoord.x + gl_FragCoord.y + 1.0) * 0.5),
-    2.0
-  );
-  #endif
-
   #ifdef GLASS
   vec4 reflexSample = texture2D(
     glassReflex,
-    vUv * 0.75 + viewDir.xy * vec2(-0.25, 0.25) + 0.125
+    vUv * 0.75 + vViewDirection.xy * vec2(-0.25, 0.25) + vec2(0.125)
   );
-  if (reflexSample.a > 0.0)
+  if (reflexSample.a > 0.0) {
     gl_FragColor.rgb = mix(gl_FragColor.rgb, reflexSample.rgb, 0.075);
+  }
   gl_FragColor.a *= pattern;
   #endif
 
@@ -195,24 +240,29 @@ void main() {
   gl_FragColor.a *= pattern * uGodrayOpacity * uGodrayDensity;
   #endif
 
+  // Fog
   #ifdef FOG
-  float fogAmount = fogDensity * min(vMvPosition.z + fogDepth, 0.0);
-  fogAmount *= fogAmount;
-  gl_FragColor.rgb = mix(
-    gl_FragColor.rgb,
-    fogColor,
-    clamp(1.0 - exp(-fogAmount), 0.0, 1.0)
-  );
+  float fogDepthValue = min(vMvPosition.z + fogDepth, 0.0);
+  float fogFactor =
+    1.0 - exp(-fogDensity * fogDensity * fogDepthValue * fogDepthValue);
+
+  fogFactor = clamp(fogFactor, 0.0, 1.0);
+  gl_FragColor.rgb = mix(gl_FragColor.rgb, fogColor, fogFactor);
   #endif
 
-  if (inspectingEnabled && inspectingFactor <= 0.0)
+  if (inspectingEnabled && !(inspectingFactor > 0.0)) {
     gl_FragColor.rgb *= 1.0 - fadeFactor;
+  }
 
   #ifdef MATCAP
-  if (glassMatcap) gl_FragColor.a *= pattern * inspectingFactor;
+  if (glassMatcap) {
+    gl_FragColor.a *= pattern * inspectingFactor;
+  }
   #endif
 
   #ifdef DAYLIGHT
-  if (daylight) gl_FragColor.a = inspectingFactor;
+  if (daylight) {
+    gl_FragColor.a = inspectingFactor;
+  }
   #endif
 }
