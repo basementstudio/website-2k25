2caaf328ad9d22af1309c529f3f82eba0921616a
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-20 12:24:15 -0300

    Minor fixes

--- src/components/layout/contact.tsx
+++ src/components/layout/contact.tsx
@@ -1,26 +1,24 @@
 import { Link } from "@/components/primitives/link"
 
-export const Contact = () => {
-  return (
-    <div className="grid-layout pb-32 pt-16">
-      <div className="relative col-span-full grid h-fit grid-cols-4 gap-2 !px-0 lg:col-span-10 lg:col-start-3 lg:grid-cols-10">
-        <h3 className="text-f-h3-mobile lg:text-f-h3 col-span-2 mb-2 text-brand-g1">
-          Contact
-        </h3>
+export const Contact = () => (
+  <div className="grid-layout pb-32 pt-16">
+    <div className="relative col-span-full grid h-fit grid-cols-4 gap-2 !px-0 lg:col-span-10 lg:col-start-2 lg:grid-cols-10 2xl:col-start-3">
+      <h3 className="text-f-h3-mobile lg:text-f-h3 col-span-2 mb-2 text-brand-g1">
+        Contact
+      </h3>
 
-        <p className="col-span-8 row-start-2 text-mobile-h1 text-brand-w2 lg:text-h1">
-          Let&apos;s make an impact together.
-        </p>
+      <div className="with-diagonal-lines pointer-events-none !absolute inset-0" />
 
-        <Link
-          href="mailto:hello@basement.studio"
-          className="col-span-5 row-start-3 text-mobile-h1 text-brand-w1 lg:text-h1"
-        >
-          <span className="actionable">hello@basement.studio</span>
-        </Link>
+      <p className="relative col-span-8 row-start-2 text-f-h1-mobile text-brand-w2 lg:text-f-h1">
+        Let&apos;s make an impact together.
+      </p>
 
-        <div className="with-diagonal-lines pointer-events-none !absolute inset-0" />
-      </div>
+      <Link
+        href="mailto:hello@basement.studio"
+        className="relative col-span-5 row-start-3 text-f-h1-mobile text-brand-w1 lg:text-f-h1"
+      >
+        <span className="actionable">hello@basement.studio</span>
+      </Link>
     </div>
-  )
-}
+  </div>
+)
