51ba5d8017dd65b4be9d7c013711402eab8671c8
git-chad <tobimocc@gmail.com>
2025-03-21 13:52:32 -0300

    final touches

--- src/app/(pages)/people/open-positions.tsx
+++ src/app/(pages)/people/open-positions.tsx
@@ -6,21 +6,21 @@ import { QueryType } from "./query"
 
 export const OpenPositions = ({ data }: { data: QueryType }) => (
   <section className="grid-layout !gap-y-4 pb-24 lg:!gap-y-2">
-    <h2 className="relative z-20 col-span-full text-f-h1-mobile text-brand-w2 lg:col-start-1 lg:row-start-1 lg:text-f-h1 xl:col-end-4">
+    <h2 className="relative z-20 col-span-full text-f-h1-mobile text-brand-w2 lg:col-start-1 lg:row-start-1 lg:text-f-h1 xl:col-end-5">
       Open <br className="hidden xl:block" />
       Positions
     </h2>
 
     <ul className="relative col-span-full mt-2 text-brand-w2 lg:row-start-2 lg:mt-0 xl:row-start-1">
       <li className="group relative grid grid-cols-12 gap-2">
-        <div className="relative col-span-full grid grid-cols-8 items-center gap-2 pb-2 text-brand-g1 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w2/20 lg:col-start-1 lg:col-end-13 xl:col-start-4">
-          <span className="col-start-1 col-end-4 text-f-h4-mobile lg:text-f-h3">
+        <div className="relative col-span-full grid grid-cols-8 items-center gap-2 pb-2 text-brand-g1 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w2/20 lg:col-start-1 lg:col-end-13 xl:col-start-5">
+          <span className="col-start-1 col-end-5 text-f-p-mobile lg:text-f-h3">
             Role
           </span>
-          <span className="col-start-4 col-end-7 text-f-h4-mobile lg:text-f-h3">
+          <span className="col-start-5 col-end-7 text-f-p-mobile lg:text-f-h3">
             Type
           </span>
-          <span className="hidden text-f-h4-mobile lg:inline-block lg:text-f-h3">
+          <span className="hidden text-f-p-mobile lg:inline-block lg:text-f-h3">
             Location
           </span>
         </div>
@@ -29,7 +29,7 @@ export const OpenPositions = ({ data }: { data: QueryType }) => (
         ({ _title, type, location, isOpen, applyUrl }, idx) => (
           <li
             key={idx}
-            className="lg:min-h-auto group relative grid min-h-16 grid-cols-12 gap-2"
+            className="group relative grid min-h-16 grid-cols-12 gap-2 lg:min-h-max"
           >
             <div
               className={cn(
@@ -44,21 +44,21 @@ export const OpenPositions = ({ data }: { data: QueryType }) => (
               href={applyUrl ?? ""}
               target="_blank"
               className={cn(
-                "relative col-span-full grid grid-cols-8 items-start gap-2 py-4 transition-colors duration-300 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w2/20 hover:text-brand-w1 lg:items-center lg:py-2 xl:col-start-4 xl:col-end-13",
+                "relative col-span-full grid grid-cols-8 items-start gap-2 py-4 transition-colors duration-300 after:absolute after:bottom-0 after:left-0 after:w-full after:border-b after:border-brand-w2/20 hover:text-brand-w1 lg:items-center lg:py-2 xl:col-start-5 xl:col-end-13",
                 { "pointer-events-none text-brand-w2/30": !isOpen }
               )}
             >
-              <span className="col-start-1 col-end-4 text-f-p-mobile lg:text-f-h3">
+              <span className="col-start-1 col-end-5 text-f-p-mobile lg:text-f-h3">
                 {_title}
               </span>
-              <div className="col-start-4 col-end-7 flex flex-col text-f-p-mobile lg:text-f-h4">
+              <div className="col-start-5 col-end-7 flex flex-col text-f-p-mobile lg:text-f-p">
                 <p>{type}</p>
               </div>
               <div className="col-start-7 col-end-9 flex w-full items-center justify-end lg:justify-between">
-                <span className="hidden text-f-p-mobile lg:inline-block lg:text-f-h4">
+                <span className="hidden text-f-p-mobile lg:inline-block lg:text-f-p">
                   {location}
                 </span>
-                <p className="actionable mt-[3px] gap-1 text-f-p-mobile lg:mt-0 lg:text-f-h4">
+                <p className="actionable mt-[3px] gap-1 text-f-p-mobile lg:mt-0 lg:text-f-p">
                   {!isOpen ? (
                     "(closed)"
                   ) : (
--- src/app/(pages)/showcase/list.tsx
+++ src/app/(pages)/showcase/list.tsx
@@ -55,7 +55,7 @@ const AccordionListItem = memo(
               <p>{project.client?._title}</p>
             </div>
 
-            <p className="relative col-start-5 col-end-11 inline-flex flex-wrap text-pretty text-f-h4 leading-none text-brand-w2">
+            <p className="relative col-start-5 col-end-11 inline-flex flex-wrap text-pretty text-f-p leading-none text-brand-w2">
               {project.categories?.map((cat, idx) => (
                 <span key={cat._title}>
                   {cat._title}
@@ -65,12 +65,12 @@ const AccordionListItem = memo(
                 </span>
               ))}
             </p>
-            <p className="relative col-start-11 col-end-12 text-left text-f-h4 text-brand-w2">
+            <p className="relative col-start-11 col-end-12 text-left text-f-p text-brand-w2">
               {project.year}
             </p>
             <Link
               href={`/showcase/${project?._slug}`}
-              className="view-project relative col-start-12 col-end-13 space-x-px text-right text-f-h4 text-brand-w2"
+              className="view-project relative col-start-12 col-end-13 space-x-px text-right text-f-p text-brand-w2"
             >
               <span className="actionable actionable-no-underline gap-x-1">
                 <span className="actionable actionable-inanimate">
