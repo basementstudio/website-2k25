2e9487d7f23f684796d551f07eee8737b857a255
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-21 17:47:44 -0300

    Minor

--- src/components/layout/contact.tsx
+++ src/components/layout/contact.tsx
@@ -3,11 +3,11 @@ import { Link } from "@/components/primitives/link"
 export const Contact = () => (
   <div className="grid-layout pb-16 pt-12 lg:pb-32 lg:pt-16">
     <div className="relative col-span-full grid h-fit grid-cols-4 gap-2 !px-0 lg:col-span-10 lg:col-start-2 lg:grid-cols-10 2xl:col-start-3">
-      <h3 className="text-f-h3-mobile lg:text-f-h3 col-span-2 mb-2 text-brand-g1">
+      <div className="with-diagonal-lines pointer-events-none !absolute inset-0" />
+
+      <h3 className="text-f-h3-mobile lg:text-f-h3 relative col-span-2 mb-2 text-brand-g1">
         Contact
       </h3>
-
-      <div className="with-diagonal-lines pointer-events-none !absolute inset-0" />
 
       <p className="relative col-span-8 row-start-2 text-f-h1-mobile text-brand-w2 lg:text-f-h1">
         Let&apos;s make an impact together.
