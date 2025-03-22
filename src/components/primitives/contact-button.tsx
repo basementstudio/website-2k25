c94cba167397731c3a5c284f4cbd683cfb17daa0
tomasferreras <tomas@basement.studio>
2025-03-22 14:50:11 +0000

    prevent contact open on load

--- src/components/primitives/contact-button.tsx
+++ src/components/primitives/contact-button.tsx
@@ -2,17 +2,19 @@ import { useRouter } from "next/navigation"
 import { useContactStore } from "../contact/contact-store"
 import { useWebgl } from "@/hooks/use-webgl"
 import { useCallback, useRef } from "react"
+import { useAppLoadingStore } from "../loading/app-loading-handler"
 
 export const ContactButton = ({ children }: { children: React.ReactNode }) => {
   const setIsContactOpen = useContactStore((state) => state.setIsContactOpen)
   const isContactOpen = useContactStore((state) => state.isContactOpen)
   const isAnimating = useContactStore((state) => state.isAnimating)
+  const canRunMainApp = useAppLoadingStore((state) => state.canRunMainApp)
   const router = useRouter()
   const webglEnabled = useWebgl()
   const clickTimeoutRef = useRef<NodeJS.Timeout | null>(null)
 
   const handleClick = useCallback(() => {
-    if (clickTimeoutRef.current || isAnimating) return
+    if (clickTimeoutRef.current || isAnimating || !canRunMainApp) return
 
     if (webglEnabled) {
       setIsContactOpen(!isContactOpen)
@@ -23,7 +25,14 @@ export const ContactButton = ({ children }: { children: React.ReactNode }) => {
     } else {
       router.push("/contact")
     }
-  }, [webglEnabled, isContactOpen, setIsContactOpen, router, isAnimating])
+  }, [
+    webglEnabled,
+    isContactOpen,
+    setIsContactOpen,
+    router,
+    isAnimating,
+    canRunMainApp
+  ])
 
   return <button onClick={handleClick}>{children}</button>
 }
