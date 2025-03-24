b76a4c58cf8c6b9f1f1d0bc5cff4d6efd6b974f1
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-03-23 22:53:16 -0300

    fix safari page transition mask affecting images

--- src/components/transitions/index.tsx
+++ src/components/transitions/index.tsx
@@ -8,16 +8,16 @@ export const Transitions = () => {
   useEffect(() => {
     document.documentElement.dataset.disabled = "true"
 
-    const handleScroll = () => {
-      const scrolled = window.scrollY
-      const viewportHeight = window.innerHeight
-      document.documentElement.dataset.disabled =
-        scrolled <= viewportHeight ? "true" : "false"
-    }
+    // const handleScroll = () => {
+    //   const scrolled = window.scrollY
+    //   const viewportHeight = window.innerHeight
+    //   document.documentElement.dataset.disabled =
+    //     scrolled <= viewportHeight ? "true" : "false"
+    // }
 
-    window.addEventListener("scroll", handleScroll, { passive: true })
+    // window.addEventListener("scroll", handleScroll, { passive: true })
 
-    return () => window.removeEventListener("scroll", handleScroll)
+    // return () => window.removeEventListener("scroll", handleScroll)
   }, [])
 
   return null
