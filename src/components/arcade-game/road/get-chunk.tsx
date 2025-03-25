df6435d8e13939c40aa457cba0f6a3a49806b2f1
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-03-25 12:23:55 -0300

    fix type

--- src/components/arcade-game/road/get-chunk.tsx
+++ src/components/arcade-game/road/get-chunk.tsx
@@ -1,10 +1,10 @@
-import { Chunk } from "./use-road"
+import { Chunk, ChunnkProps } from "./use-road"
 import { BaseRoad } from "./chunks/base-road"
 
 export const getNewChunk = (): Chunk => {
   const randomId = crypto.randomUUID()
   return {
     id: randomId,
-    Component: BaseRoad // Todo: create more chunks and pick one randomly
+    Component: BaseRoad as (props: ChunnkProps) => React.ReactElement
   }
 }
