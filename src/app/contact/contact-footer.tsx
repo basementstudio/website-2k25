53d092e89a3ab88c6beb5ef33fa8af51f27a336d
Tomas Ferreras <hellotomasdev@gmail.com>
2025-03-25 12:25:05 -0300

    Enhance contact footer with new social media links and open in new tabs (#308)

--- src/app/contact/contact-footer.tsx
+++ src/app/contact/contact-footer.tsx
@@ -1,32 +1,35 @@
+import { Pump } from "basehub/react-pump"
+import { query } from "@/components/layout/query"
+import { SocialLinks } from "@/components/layout/shared-sections"
+
 export const ContactFooter = () => (
-  <footer className="col-span-4 row-start-2 mt-auto flex flex-col justify-end gap-6 xl:mt-0">
-    <a
-      href="mailto:hello@basement.studio"
-      className="w-fit text-[30px] font-semibold not-italic leading-none tracking-[-2.16px] xl:text-[56px] xl:tracking-[-2.24px]"
-    >
-      <span className="actionable actionable-no-underline group !inline">
-        <span className="custom-underline">hello@</span>
-        <br />
-        <span className="custom-underline">basement.</span>
-        <br />
-        <span className="custom-underline">studio</span>
-      </span>
-    </a>
-    <nav
-      className="flex items-center gap-2 text-f-h3-mobile lg:text-f-h3"
-      aria-label="Social media links"
-    >
-      <a href="https://twitter.com/basementstudio" title="Twitter">
-        x (twitter)
-      </a>
-      <span className="text-brand-g1">,</span>
-      <a href="https://www.instagram.com/basementdotstudio" title="Instagram">
-        instagram
-      </a>
-      <span className="text-brand-g1">,</span>
-      <a href="https://github.com/basementstudio" title="GitHub">
-        github
-      </a>
-    </nav>
-  </footer>
+  <Pump queries={[query]}>
+    {async ([data]) => {
+      "use server"
+
+      return (
+        <footer className="col-span-4 row-start-2 mt-auto flex flex-col justify-end gap-6 xl:mt-0">
+          <a
+            href="mailto:hello@basement.studio"
+            className="w-fit text-[30px] font-semibold not-italic leading-none tracking-[-2.16px] xl:text-[56px] xl:tracking-[-2.24px]"
+          >
+            <span className="actionable actionable-no-underline group !inline">
+              <span className="custom-underline">hello@</span>
+              <br />
+              <span className="custom-underline">basement.</span>
+              <br />
+              <span className="custom-underline">studio</span>
+            </span>
+          </a>
+          <SocialLinks
+            links={{
+              ...data.company.social,
+              linkedIn: data.company.social.linkedIn || ""
+            }}
+            className="flex items-center gap-2 text-f-h3-mobile lg:text-f-h3"
+          />
+        </footer>
+      )
+    }}
+  </Pump>
 )
