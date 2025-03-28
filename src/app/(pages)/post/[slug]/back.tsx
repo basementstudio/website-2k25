3cf07479ff6ccc4a13b2a1dc2c965325d2492bd8
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-28 13:16:06 -0300

    minor

--- src/app/(pages)/post/[slug]/back.tsx
+++ src/app/(pages)/post/[slug]/back.tsx
@@ -5,9 +5,10 @@ import { Arrow } from "@/components/primitives/icons/arrow"
 export const Back = () => (
   <Link
     href="/blog#list"
-    className="actionable h-3 text-f-p-mobile text-brand-w1 lg:!sticky lg:top-13 lg:text-f-p"
+    className="actionable text-f-p-mobile text-brand-w1 lg:!sticky lg:top-13 lg:text-f-p"
   >
-    <Arrow className="mr-1 size-4 rotate-180" />
-    Blog
+    <span className="actionable inline-flex items-center gap-1">
+      <Arrow className="size-4 rotate-180" /> Blog
+    </span>
   </Link>
 )
