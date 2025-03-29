690774c2ac9a910514b53bbb20c5b78c40dd1cd0
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-03-29 20:51:03 -0300

    Update mobile nav behavior

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,8 +1,5 @@
-import { Navbar } from "@/components/layout/navbar"
-
 const ContactLayout = ({ children }: { children: React.ReactNode }) => (
   <>
-    <Navbar />
     <main className="relative flex h-[100svh] flex-col bg-brand-k after:absolute after:-top-px after:z-10 after:h-px after:w-full after:bg-brand-w1/10">
       {children}
     </main>
