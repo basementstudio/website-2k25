557b2a947cd599911cebe42ecbb8e080d1fcbee5
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-03 11:26:39 -0300

    fix

--- src/app/lab/client.tsx
+++ src/app/lab/client.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 import { useAppLoadingStore } from "@/components/loading/app-loading-handler"
