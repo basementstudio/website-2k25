9ccb2a836226ff951fde74dd2596de0863e8c561
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-09 12:37:28 -0300

    upd

--- src/components/perf/index.tsx
+++ src/components/perf/index.tsx
@@ -3,15 +3,14 @@ import { useSearchParams } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
 import { ThreePerf } from "three-perf"
 
-import { useFrameCallback } from "@/hooks/use-pausable-time"
+import { getPerf, setPerf } from "@/hooks/use-perf"
 
 export const PerfMonitor = () => {
   const searchParams = useSearchParams()
   const [debug, setDebug] = useState(false)
   const perfRef = useRef<ThreePerf | undefined>(undefined)
   const gl = useThree((s) => s.gl)
-  const scene = useThree((s) => s.scene)
-  const camera = useThree((s) => s.camera)
+  const { instance } = getPerf()
 
   useEffect(() => {
     const hasDebg = searchParams.has("debug")
@@ -20,27 +19,21 @@ export const PerfMonitor = () => {
   }, [searchParams])
 
   useEffect(() => {
+    if (instance) return
     perfRef.current = new ThreePerf({
       anchorX: "right",
       anchorY: "bottom",
       domElement: document.body, // or other canvas rendering wrapper
       renderer: gl // three js renderer instance you use for rendering
     })
-  }, [gl])
+    setPerf({ instance: perfRef.current })
+  }, [gl, instance])
 
   useEffect(() => {
     if (!perfRef.current) return
     console.log(debug)
     perfRef.current.visible = debug
   }, [debug])
-
-  useFrameCallback(() => {
-    if (!perfRef.current) return
-
-    perfRef.current.begin()
-    gl.render(scene, camera)
-    perfRef.current.end()
-  })
 
   return null
 }
--- src/components/scene/index.tsx
+++ src/components/scene/index.tsx
@@ -140,7 +140,7 @@ export const Scene = () => {
         <Debug />
         <Canvas
           id="canvas"
-          frameloop="never"
+          frameloop="demand"
           ref={canvasRef}
           tabIndex={0}
           onFocus={handleFocus}
