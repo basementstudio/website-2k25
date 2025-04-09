210de46a0e12ba905319821e4c90b41bb56e876b
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-09 14:54:57 -0300

    save perf logs

--- src/components/perf/index.tsx
+++ src/components/perf/index.tsx
@@ -1,16 +1,27 @@
+"use client"
+
 import { useThree } from "@react-three/fiber"
 import { useSearchParams } from "next/navigation"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { ThreePerf } from "three-perf"
 
-import { getPerf, setPerf } from "@/hooks/use-perf"
+import { useFrameCallback } from "@/hooks/use-pausable-time"
+import { setPerf, usePerf } from "@/hooks/use-perf"
+import { debounce } from "@/utils/debounce"
+
+const DEBOUNCE_WAIT = 5
 
 export const PerfMonitor = () => {
   const searchParams = useSearchParams()
   const [debug, setDebug] = useState(false)
   const perfRef = useRef<ThreePerf | undefined>(undefined)
   const gl = useThree((s) => s.gl)
-  const { instance } = getPerf()
+  const instance = usePerf((s) => s.instance)
+
+  const debouncedUpdatePerfStore = useMemo(
+    () => debounce(setPerf, DEBOUNCE_WAIT),
+    []
+  )
 
   useEffect(() => {
     const hasDebg = searchParams.has("debug")
@@ -27,8 +38,8 @@ export const PerfMonitor = () => {
       anchorY: "top",
       domElement: canvasContainer || document.body, // or other canvas rendering wrapper
       renderer: gl, // three js renderer instance you use for rendering
-      enabled: false,
-      logsPerSecond: 1
+      overClock: false,
+      logsPerSecond: 5
     })
     perfRef.current.ui.wrapper.style.top = "37px"
     perfRef.current.ui.wrapper.style.display = "none"
@@ -40,7 +51,6 @@ export const PerfMonitor = () => {
     const perfInstance = perfRef.current
 
     if (debug) {
-      perfInstance.enabled = true
       perfRef.current.ui.wrapper.style.display = "block"
       perfInstance.logsPerSecond = 10
     }
@@ -49,6 +59,28 @@ export const PerfMonitor = () => {
       perfInstance.enabled = false
     }
   }, [debug])
+
+  useFrameCallback(() => {
+    if (!perfRef.current) return
+    const perfInstance = perfRef.current
+
+    debouncedUpdatePerfStore(
+      (state) => ({
+        ...state,
+        log: perfInstance.log,
+        infos: perfInstance.infos,
+        startTime: perfInstance.startTime,
+        fpsLimit: perfInstance.fpsLimit,
+        overclockingFps: perfInstance.overclockingFps,
+        accumulated: perfInstance.accumulated,
+        chart: perfInstance.chart,
+        gl: perfInstance.gl,
+        objectWithMaterials: perfInstance.objectWithMaterials,
+        programs: perfInstance.programs
+      }),
+      true
+    )
+  })
 
   return null
 }
