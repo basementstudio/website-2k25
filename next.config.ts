f04730b87b1b6d3f847383abd93beacb02da1261
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-04-08 22:28:49 -0300

    Redirect old blog posts

--- next.config.ts
+++ next.config.ts
@@ -69,6 +69,7 @@ const nextConfig: NextConfig = {
 
   async redirects() {
     return [
+      // Old pages
       {
         source: "/about",
         destination: "/services",
@@ -84,6 +85,8 @@ const nextConfig: NextConfig = {
         destination: "/showcase",
         permanent: true
       },
+
+      // Old showcase pages
       {
         source: "/showcase/apollographql",
         destination: "/showcase/apollo-a-brand-built-for-liftoff",
@@ -119,9 +122,64 @@ const nextConfig: NextConfig = {
         destination:
           "/showcase/showcase/basement-chronicles-the-internet-needs-you",
         permanent: true
-      }
+      },
 
-      // Todo: make proper redirects for blog posts
+      // Old blog posts
+      {
+        source: "/blog/building-a-unique-website-for-basement-grotesque",
+        destination: "/post/building-a-unique-website-for-basement-grotesque",
+        permanent: true
+      },
+      {
+        source: "/blog/migrating-large-scale-websites-from-gatsby-to-next-js",
+        destination:
+          "/post/migrating-large-scale-websites-from-gatsby-to-nextjs",
+        permanent: true
+      },
+      {
+        source: "/blog/mastering-color-gradients",
+        destination: "/post/mastering-color-gradients",
+        permanent: true
+      },
+      {
+        source: "/blog/gsap-next-js-setup-the-bsmnt-way",
+        destination: "/post/gsap-and-nextjs-setup-the-bsmnt-way",
+        permanent: true
+      },
+      {
+        source:
+          "/blog/the-making-of-adhesion-a-bsmnt-take-on-blackletter-typefaces",
+        destination:
+          "/post/the-making-of-adhesion-a-bsmnt-take-on-blackletter-typefaces",
+        permanent: true
+      },
+      {
+        source: "/blog/kidsuper-world-bringing-paints-to-life-with-r3f",
+        destination: "/post/kidsuper-world-bringing-paints-to-life-with-r3f",
+        permanent: true
+      },
+      {
+        source: "/blog/navigating-the-future-within-the-next-js-app-router",
+        destination: "/post/navigating-the-future-within-the-nextjs-app-router",
+        permanent: true
+      },
+      {
+        source:
+          "/blog/shipping-ship-behind-the-particle-shader-effect-for-vercel-s-conf",
+        destination:
+          "/post/shipping-ship-behind-the-particle-shader-effect-for-vercels-conf",
+        permanent: true
+      },
+      {
+        source: "/blog/daylight-shadows",
+        destination: "/post/creating-daylight-or-the-shadows",
+        permanent: true
+      },
+      {
+        source: "/blog/creating-daylight-the-devex",
+        destination: "/post/creating-daylight-or-the-devex",
+        permanent: true
+      }
     ]
   }
 }
