1dcbc8fba34582ba6f59780e5feb4ed4d5254f03
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-10 16:05:55 -0300

    use pnpm

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,8 @@
 if [[ $VERCEL_ENV == "production"  ]] ; then
-  npm run build
+  pnpm run build
   curl --proto '=https' --tlsv1.2 -LsSf https://github.com/PostHog/posthog/releases/download/posthog-cli-v0.0.4/posthog-cli-installer.sh | sh
   /vercel/.posthog/posthog-cli --host https://us.posthog.com sourcemap inject --directory ./.next/static/chunks
   /vercel/.posthog/posthog-cli --host https://us.posthog.com sourcemap upload --directory ./.next/static/chunks
 else
-  npm run build
+  pnpm run build
 fi
