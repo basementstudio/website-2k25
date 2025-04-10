8e0df54c1954b7fe314a104af56d05cad8b49898
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-10 16:43:27 -0300

    Update vercel.sh to trigger build process for production environment

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $VERCEL_ENV == "preview"  ]] ; then
+if [[ $VERCEL_ENV == "production"  ]] ; then
   pnpm run build
   curl --proto '=https' --tlsv1.2 -LsSf https://github.com/PostHog/posthog/releases/download/posthog-cli-v0.0.4/posthog-cli-installer.sh | sh
   /vercel/.posthog/posthog-cli --host https://us.posthog.com sourcemap inject --directory ./.next/static/chunks
