8bea8dd57f245f1a807eafe3eb00b63b8adc5ed0
Nacho Mandagaran <33981565+ignmandagaran@users.noreply.github.com>
2025-04-10 16:55:35 -0300

    Improve error tracking stack traces (#360)
    * Add Vercel configuration and build script
    
    * use pnpm
    
    * test
    
    * Update vercel.sh to trigger build process for production environment

---
+++ vercel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $VERCEL_ENV == "production"  ]] ; then
+  pnpm run build
+  curl --proto '=https' --tlsv1.2 -LsSf https://github.com/PostHog/posthog/releases/download/posthog-cli-v0.0.4/posthog-cli-installer.sh | sh
+  /vercel/.posthog/posthog-cli --host https://us.posthog.com sourcemap inject --directory ./.next/static/chunks
+  /vercel/.posthog/posthog-cli --host https://us.posthog.com sourcemap upload --directory ./.next/static/chunks
+else
+  pnpm run build
+fi
