889a27621ec252e601a5578c660e156b83a1104b
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-12-05 11:27:42 -0300

    Refactor AssetsProviderServer to use async fetching
    - Updated the `AssetsProviderServer` component to fetch assets asynchronously using `fetchAssets`.
    - Removed the `Suspense` wrapper to streamline the component structure and improve performance.

--- src/components/assets-provider/server.tsx
+++ src/components/assets-provider/server.tsx
@@ -1,27 +1,14 @@
-import { Suspense } from "react"
-
 import { AppHooks } from "../app-hooks-init"
 import { AssetsProviderClient } from "."
 import { fetchAssets } from "./fetch-assets"
 
-export const AssetsProviderServer = ({
-  children
-}: {
-  children: React.ReactNode
-}) => {
-  return (
-    <Suspense fallback={null}>
-      <AssetsProviderFetch>{children}</AssetsProviderFetch>
-    </Suspense>
-  )
-}
-
-export const AssetsProviderFetch = async ({
+export const AssetsProviderServer = async ({
   children
 }: {
   children: React.ReactNode
 }) => {
   const assets = await fetchAssets()
+
   return (
     <AssetsProviderClient assets={assets}>
       {children}
