3ff715f84a4d169389fd5bf0cd36657a53b635d1
Vittorio Retrivi <vittorioretrivi@gmail.com>
2025-12-08 14:37:29 -0300

    Force build

--- src/hooks/use-console-logo.ts
+++ src/hooks/use-console-logo.ts
@@ -1,17 +1,3 @@
 import { useEffect } from "react"
 
-export const useConsoleLogo = () => {
-  useEffect(() => {
-    console.log(
-      `%c
-██████╗ ███████╗███╗   ███╗███╗   ██╗████████╗
-██╔══██╗██╔════╝████╗ ████║████╗  ██║╚══██╔══╝
-██████╔╝███████╗██╔████╔██║██╔██╗ ██║   ██║   
-██╔══██╗╚════██║██║╚██╔╝██║██║╚██╗██║   ██║   
-██████╔╝███████║██║ ╚═╝ ██║██║ ╚████║   ██║   
-╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝   ╚═╝   
-Making cool shit that performs.`,
-      "font-family: 'Geist Mono', 'Menlo', 'Monaco', 'Courier', monospace; font-size: 13px"
-    )
-  }, [])
-}
+export const useConsoleLogo = () => {}
