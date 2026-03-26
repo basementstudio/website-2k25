83727830935d33546aa632807daa142af81376d2
Mariana Esteban <marianavesteban@gmail.com>
2026-03-26 15:10:42 -0300

    feat: [US-006] - Define blog document schemas

---
+++ sanity/schemas/documents/postCategory.ts
@@ -0,0 +1,22 @@
+import { defineType, defineField } from 'sanity'
+
+export const postCategory = defineType({
+  name: 'postCategory',
+  title: 'Post Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,3 +1,5 @@
+import { post } from './documents/post'
+import { postCategory } from './documents/postCategory'
 import { codeBlock } from './objects/codeBlock'
 import { codeSandbox } from './objects/codeSandbox'
 import { gridGallery } from './objects/gridGallery'
@@ -8,6 +10,10 @@ import { socialNetwork } from './objects/socialNetwork'
 import { tweetEmbed } from './objects/tweetEmbed'
 
 export const schemaTypes = [
+  // Document types
+  post,
+  postCategory,
+
   // Object types
   codeBlock,
   codeSandbox,
